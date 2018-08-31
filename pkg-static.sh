addd3d0e779b3da6b0648b4d72e8076db64a6509
Douglas Naphas <douglasnaphas@gmail.com>
2018-08-31 16:17:42 -0400

    22 Added Amplify, AWS JS SDK, logging the logged-in user

--- pkg-static.sh
+++ pkg-static.sh
@@ -6,6 +6,10 @@ REAL_URL=https://294csan61i.execute-api.us-east-1.amazonaws.com/Prod
 DEV_URL=http://127.0.0.1:3000
 REAL_JS_IMPORT='<script src="https://madliberationgame.com/'
 DEV_JS_IMPORT='<script src="./'
+REAL_CALLBACK='https://madliberationfederated.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=6ktt0mtpks03r8sfticc3h1o6&redirect_uri=https://madliberationgame.com/pick-script.html'
+DEV_CALLBACK='https://madliberationfederated.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=6ktt0mtpks03r8sfticc3h1o6&redirect_uri=file:///Users/douglasnaphas/repos/madliberation/target/static/pick-script.html'
+REAL_REDIRECT=redirect_uri=https://madliberationgame.com/pick-script.html
+DEV_REDIRECT=redirect_uri=file:///Users/douglasnaphas/repos/madliberation/target/static/pick-script.html
 subs=( "${REAL_URL}"       "${DEV_URL}"       \
        "${REAL_JS_IMPORT}" "${DEV_JS_IMPORT}" )
 SOURCE_DIR=src/main/resources/static
@@ -19,5 +23,6 @@ do
 	devi="$((reali + 1))"
 	real="${subs[${reali}]}"
 	dev="${subs[${devi}]}"
+	export LC_ALL=C
 	grep -rl "${real}" "${TARGET_DIR}" | xargs sed -i '' "s~${real}~${dev}~g"
 done

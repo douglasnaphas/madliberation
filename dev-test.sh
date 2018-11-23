d294c6c32a97f19d0adb10d093c47027a28e4e91
Douglas Naphas <douglasnaphas@gmail.com>
2018-11-23 18:02:45 -0500

    Remove front-end from this repo
    madliberationjs, a React app, contains the front-end for Mad Liberation.
    
    madliberation-50

--- dev-test.sh
+++ dev-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mvn package shade:shade -P local
+mvn package shade:shade
 diff template.yml <(awk  '! /CodeUri:/' dev-test-template.yml) &> /dev/null || \
 	awk '! /CodeUri:/' dev-test-template.yml > template.yml
-sam local start-api -t ~/repos/madliberation/dev-test-template.yml
+sam local start-api --template ~/repos/madliberation/dev-test-template.yml --port 3001

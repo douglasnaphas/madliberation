1e4b1c3ce9d0afb07bfe502344a3a1994096b7e5
Douglas Naphas <douglasnaphas@gmail.com>
2020-11-23 23:03:29 -0500

    Attempt to deploy mljsapi to API Gateway
    https://github.com/douglasnaphas/madliberation/issues/254

--- lib/madliberation-stack.ts
+++ lib/madliberation-stack.ts
@@ -1,18 +1,34 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as sns from "@aws-cdk/aws-sns";
+import * as subs from "@aws-cdk/aws-sns-subscriptions";
+import * as sqs from "@aws-cdk/aws-sqs";
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class MadliberationStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'MadliberationQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const queue = new sqs.Queue(this, "MadliberationQueue", {
+      visibilityTimeout: cdk.Duration.seconds(300),
     });
 
-    const topic = new sns.Topic(this, 'MadliberationTopic');
+    const topic = new sns.Topic(this, "MadliberationTopic");
 
     topic.addSubscription(new subs.SqsSubscription(queue));
+
+    const fn = new lambda.Function(this, "HitCounterHandler", {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset("mljsapi"),
+      environment: {
+        NODE_ENV: "development",
+      },
+      timeout: cdk.Duration.seconds(20),
+    });
+
+    const lambdaApi = new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: fn,
+    });
   }
 }

6262459040df133a310a42a75f44fe3f2e3e115d
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-24 15:41:55 -0800

    Trim quotes from bucket name
    The frontend bucket name is saved in SSM param store with quotes.

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -2,7 +2,7 @@
 
 # get the bucket name from SSM param store
 PARAM_NAME=$(npx @cdk-turnkey/stackname --suffix FrontendBucketName)
-BUCKET=$(aws ssm get-parameter --name ${PARAM_NAME} | jq '.Parameter.Value')
+BUCKET=$(aws ssm get-parameter --name ${PARAM_NAME} | jq '.Parameter.Value' | tr -d '"')
 
 echo "PARAM_NAME:"
 echo "${PARAM_NAME}"

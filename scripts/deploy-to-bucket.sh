3c5edf80725f2b2f682e0201525dcf62050686f5
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-24 15:34:50 -0800

    Supply --suffix when grabbing bucket name

--- scripts/deploy-to-bucket.sh
+++ scripts/deploy-to-bucket.sh
@@ -2,6 +2,16 @@
 
 deploy-to-bucket() {
   BUCKET=$1
+  if [[ ! -n "${BUCKET}" ]]
+  then
+    echo "error, deploy-to-bucket: bad bucket name ${BUCKET}"
+    exit 1
+  fi
+  if ! aws s3 ls "s3://${BUCKET}" &> /dev/null
+  then
+    echo "error, deploy-to-bucket: unable to ls on s3://${BUCKET}"
+    exit 1
+  fi
   cd madliberationjs
   aws s3 sync --content-type "text/html" --exclude "*" --include "*.html" --delete build/ s3://${BUCKET}/
   aws s3 sync --content-type "text/css" --exclude "*" --include "*.css" --include "*.css.map" --delete build/ s3://${BUCKET}/

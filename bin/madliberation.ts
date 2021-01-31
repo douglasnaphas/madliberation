6c414dc9604fc212c956c590f3c0495dff94c01e
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-31 13:55:34 -0800

    Use stackname to name the stack
    https://github.com/douglasnaphas/madliberation/issues/260
    
    This will allow multiple instances of the application to live in the
    same account, which is critical for the desired development workflow.

--- bin/madliberation.ts
+++ bin/madliberation.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { MadliberationStack } from '../lib/madliberation-stack';
+import * as cdk from "@aws-cdk/core";
+import { MadliberationStack } from "../lib/madliberation-stack";
+const stackname = require("@cdk-turnkey/stackname");
 
 const app = new cdk.App();
-new MadliberationStack(app, 'MadliberationStack');
+new MadliberationStack(app, stackname("webapp"));

048d054cd5d78fc56f185b6b65c584b0a72975dd
Douglas Naphas <douglasnaphas@gmail.com>
2021-02-05 19:33:49 -0800

    Add an API smoke test
    https://github.com/douglasnaphas/madliberation/issues/272
    
    This will probably fail, since for some reason I'm getting a CloudFront
    error ever since my recent change to disable caching and use an
    all-viewer Origin Request Policy.
    
    If this does fail I'll try with the all-viewer policy removed.

---
+++ scripts/itest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+STACKNAME=$(npx @cdk-turnkey/stackname@1.1.0 --suffix webapp)
+URL=https://$(aws cloudformation describe-stacks \
+  --stack-name ${STACKNAME} | \
+  jq '.Stacks[0].Outputs | map(select(.OutputKey == "DomainName"))[0].OutputValue' | \
+  tr -d \")
+API_CANARY_URL=${URL}/prod/public-endpoint
+CANARY_OUTPUT=$(curl ${API_CANARY_URL} | jq '.Output')
+if [[ "${CANARY_OUTPUT}" != "\"this endpoint is public\"" ]]
+then
+  echo "expected output from ${API_CANARY_URL} to be \"this endpoint is public\""
+  echo "got:"
+  echo "${CANARY_OUTPUT}"
+  echo "failing"
+  exit 1
+fi
+# npx ../itest --site ${URL}

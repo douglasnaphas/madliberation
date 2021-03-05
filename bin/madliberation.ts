c7165837ed5b0d81689988278acbab4e9cc2a37b
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-05 17:49:29 -0500

    Begin verification of the signup email address
    gh-281

--- bin/madliberation.ts
+++ bin/madliberation.ts
@@ -21,6 +21,8 @@ const stackname = require("@cdk-turnkey/stackname");
   const ssmParams = {
     Names: [emailVerificationAddressParam, emailVerificationRegionParam],
   };
+  // TODO: Either remove this region constraint, or enforce it for the from
+  // address as well.
   AWS.config.update({ region: "us-east-1" });
   const ssm = new AWS.SSM();
   let ssmResponse: any;
@@ -41,13 +43,53 @@ const stackname = require("@cdk-turnkey/stackname");
         }
       }
     );
+    if (!sesEmailVerificationFromAddress) {
+      console.log(
+        "error: sesEmailVerificationFromAddress expected, but not set"
+      );
+      console.log("to fix this, set this param:");
+      console.log(emailVerificationAddressParam);
+      console.log("or leave it unset, and unset this one:");
+      console.log(emailVerificationRegionParam);
+      process.exit(1);
+    }
+    if (!sesEmailVerificationFromRegion) {
+      console.log(
+        "error: sesEmailVerificationFromRegion expected, but not set"
+      );
+      console.log("to fix this, set this param:");
+      console.log(emailVerificationRegionParam);
+      console.log("or leave it unset, and unset this one:");
+      console.log(emailVerificationAddressParam);
+      process.exit(1);
+    }
+
+    // We have the verification email info, now validate it
+    const sesv2 = new AWS.SESV2({ apiVersion: "2019-09-27" });
+    // Check to make sure the email is verified and has sending enabled
+    let sesv2Response: any;
+    const getEmailIdentityParams = {
+      EmailIdentity: sesEmailVerificationFromAddress,
+    };
+    sesv2Response = await new Promise((resolve, reject) => {
+      sesv2.getEmailIdentity(getEmailIdentityParams, (err: any, data: any) => {
+        resolve({ err, data });
+      });
+    });
+    if (sesv2Response.err) {
+      console.log("error: Could not get email identity, tried to get:");
+      console.log(sesEmailVerificationFromAddress);
+    }
+
+    // Check to make sure the domain is DKIM-enabled
   }
+
   new MadliberationUe1(app, stackname("ue1"), {
     env: { region: "us-east-1" },
     sesVerificationConfig: sesEmailVerificationFromAddress &&
       sesEmailVerificationFromRegion && {
         fromAddress: sesEmailVerificationFromAddress,
-        fromRegion: sesEmailVerificationFromRegion,
+        fromRegion: sesEmailVerificationFromRegion, // prob. must be ue1 for now
       },
   });
   new MadliberationWebapp(app, stackname("webapp"));

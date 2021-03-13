f56f7a741b9ffd9c85b2d4a66dcd53812523f304
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-13 15:01:12 -0500

    Try to get a screenshot before the test failure

--- scripts/itest.sh
+++ scripts/itest.sh
@@ -53,8 +53,15 @@ USER_POOL_DOMAIN=$(aws cognito-idp describe-user-pool \
   tr -d \")
 REDIRECT_URI=${APP_URL}/prod/get-cookies
 IDP_URL="https://${USER_POOL_DOMAIN}.auth.${AWS_DEFAULT_REGION}.amazoncognito.com/login?response_type=code&client_id=${USER_POOL_CLIENT_ID}&redirect_uri=${REDIRECT_URI}"
+echo "APP_URL:"
+echo ${APP_URL}
+echo "IDP_URL:"
+echo ${IDP_URL}
+echo "USER_POOL_ID:"
+echo ${USER_POOL_ID}
 npx ../itest \
   --site ${APP_URL} \
   --idp-url "${IDP_URL}" \
   --user-pool-id ${USER_POOL_ID}
-  
+pwd
+ls

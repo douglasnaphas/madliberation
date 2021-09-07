957713930d2d3008690d593d5700600ba3ec278f
Douglas Naphas <douglasnaphas@gmail.com>
2021-09-07 00:16:27 -0400

    Apply Prettier settings to YourRoomCodePage.test

--- frontend/src/components/YourRoomCodePage.test.js
+++ frontend/src/components/YourRoomCodePage.test.js
@@ -1,8 +1,8 @@
-import { createMount } from '@material-ui/core/test-utils';
-import { MemoryRouter } from 'react-router-dom';
-import React from 'react';
+import { createMount } from "@material-ui/core/test-utils";
+import { MemoryRouter } from "react-router-dom";
+import React from "react";
 
-import YourRoomCodePage from './YourRoomCodePage';
+import YourRoomCodePage from "./YourRoomCodePage";
 
 let mount;
 beforeEach(() => {
@@ -12,34 +12,34 @@ afterEach(() => {
   mount.cleanUp();
 });
 
-const getProps = ({ joinSeder, roomCode, gameName }) => {
+const makeProps = ({ joinSeder, roomCode, gameName }) => {
   const props = {
     joinSeder: jest.fn(),
     setConfirmedRoomCode: jest.fn(),
     setConfirmedGameName: jest.fn(),
     confirmedRoomCode: roomCode || false,
-    confirmedGameName: gameName
+    confirmedGameName: gameName,
   };
-  if (typeof joinSeder === 'function') {
+  if (typeof joinSeder === "function") {
     props.joinSeder = jest.fn(joinSeder);
   }
   return props;
 };
 
-describe('YourRoomCodePage', () => {
-  test('confirmedRoomCode not received', () => {
-    const props = getProps({});
+describe("YourRoomCodePage", () => {
+  test("confirmedRoomCode not received", () => {
+    const props = makeProps({});
     const wrapper = mount(
       <MemoryRouter>
         <YourRoomCodePage {...props}></YourRoomCodePage>
       </MemoryRouter>
     );
   });
-  test('chosenPath not received', () => {});
-  test('confirmedRoomCode should be present in top bar', () => {});
+  test("chosenPath not received", () => {});
+  test("confirmedRoomCode should be present in top bar", () => {});
   test("that's my name button should be disabled when input empty", () => {});
-  test('one character of input should enable the button', () => {});
-  test('several characters of input should leave the button enabled', () => {});
-  test('the button should return to disabled after deleted input', () => {});
-  test('button should be disabled during join attempt', () => {});
+  test("one character of input should enable the button", () => {});
+  test("several characters of input should leave the button enabled", () => {});
+  test("the button should return to disabled after deleted input", () => {});
+  test("button should be disabled during join attempt", () => {});
 });

e0a90aab253c30d22a226cecab5fe16aa48fe404
Douglas Naphas <douglasnaphas@gmail.com>
2022-02-25 18:56:36 -0500

    Rewrite my first test in React Testing Library
    gh-388

--- frontend/src/components/ScriptTable.test.js
+++ frontend/src/components/ScriptTable.test.js
@@ -1,20 +1,22 @@
-import { Button } from '@material-ui/core';
-import { createMount } from '@material-ui/core/test-utils';
-import { Link } from 'react-router-dom';
-import { MemoryRouter } from 'react-router-dom';
-import React from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
-import Radio from '@material-ui/core/Radio';
+import { Button } from "@material-ui/core";
+import { createMount } from "@material-ui/core/test-utils";
+import { Link } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Radio from "@material-ui/core/Radio";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-import ScriptTable from './ScriptTable';
+import ScriptTable from "./ScriptTable";
 import {
   testUtils,
   expectSelectorCount,
-  expectFewerThanNOfSelector
-} from '../test/testUtils';
+  expectFewerThanNOfSelector,
+} from "../test/testUtils";
 
 let mount;
 beforeEach(() => {
@@ -31,9 +33,9 @@ afterEach(() => {
  */
 function selectScript(wrapper, scriptNumber) {
   wrapper
-    .findWhere(n => n.is(Radio) && n.is(`#script-${scriptNumber}`))
-    .prop('onChange')({
-    target: { value: `${scriptNumber}` }
+    .findWhere((n) => n.is(Radio) && n.is(`#script-${scriptNumber}`))
+    .prop("onChange")({
+    target: { value: `${scriptNumber}` },
   });
   wrapper.update();
 }
@@ -47,8 +49,8 @@ function selectScript(wrapper, scriptNumber) {
 function scriptChecked(wrapper, scriptNumber) {
   return wrapper
     .findWhere(
-      n =>
-        n.is(Radio) && n.is(`#script-${scriptNumber}`) && n.is('[checked=true]')
+      (n) =>
+        n.is(Radio) && n.is(`#script-${scriptNumber}`) && n.is("[checked=true]")
     )
     .exists();
 }
@@ -79,98 +81,98 @@ function scriptCheckedOnly(wrapper, selectedScriptNumber, scriptCount) {
 function fourScripts() {
   return [
     {
-      haggadah_description: 'An unoffensive script for the whole family',
-      lib_id: 'script#0001',
-      haggadah_short_desc: 'Family Script',
-      room_code: 'AAAAAA',
-      path: 'madliberation-scripts/001-Family_Script',
+      haggadah_description: "An unoffensive script for the whole family",
+      lib_id: "script#0001",
+      haggadah_short_desc: "Family Script",
+      room_code: "AAAAAA",
+      path: "madliberation-scripts/001-Family_Script",
       is_script: 1,
-      haggadah_name: '0001 - Family Script',
-      script_number: 1
+      haggadah_name: "0001 - Family Script",
+      script_number: 1,
     },
     {
-      haggadah_description: 'An offensive script for only part of the family',
-      lib_id: 'script#0002',
-      haggadah_short_desc: 'Dirty Script',
-      room_code: 'AAAAAB',
-      path: 'madliberation-scripts/002-Dirty_Script',
+      haggadah_description: "An offensive script for only part of the family",
+      lib_id: "script#0002",
+      haggadah_short_desc: "Dirty Script",
+      room_code: "AAAAAB",
+      path: "madliberation-scripts/002-Dirty_Script",
       is_script: 1,
-      haggadah_name: '0002 - Dirty Script',
-      script_number: 2
+      haggadah_name: "0002 - Dirty Script",
+      script_number: 2,
     },
     {
-      haggadah_description: 'An offensive script for the whole family',
-      lib_id: 'script#0003',
-      haggadah_short_desc: 'Dirty Family Script',
-      room_code: 'AAAAAC',
-      path: 'madliberation-scripts/003-Dirty_Family_Script',
+      haggadah_description: "An offensive script for the whole family",
+      lib_id: "script#0003",
+      haggadah_short_desc: "Dirty Family Script",
+      room_code: "AAAAAC",
+      path: "madliberation-scripts/003-Dirty_Family_Script",
       is_script: 1,
-      haggadah_name: '0003 - Dirty Family Script',
-      script_number: 3
+      haggadah_name: "0003 - Dirty Family Script",
+      script_number: 3,
     },
     {
-      haggadah_description: 'A script to help you get good at Mad Liberation',
-      lib_id: 'script#0004',
-      haggadah_short_desc: 'Practice Script',
-      room_code: 'AAAAAD',
-      path: 'madliberation-scripts/004-Practice_Script',
+      haggadah_description: "A script to help you get good at Mad Liberation",
+      lib_id: "script#0004",
+      haggadah_short_desc: "Practice Script",
+      room_code: "AAAAAD",
+      path: "madliberation-scripts/004-Practice_Script",
       is_script: 1,
-      haggadah_name: '0004 - Practice Script',
-      script_number: 4
-    }
+      haggadah_name: "0004 - Practice Script",
+      script_number: 4,
+    },
   ];
 }
 function differentScripts() {
   return [
     {
       haggadah_description:
-        'An unoffensive script for the whole different family',
-      lib_id: 'script#0005',
-      haggadah_short_desc: 'Different Family Script',
-      room_code: 'AAAAAE',
-      path: 'madliberation-scripts/005-Family_Script',
+        "An unoffensive script for the whole different family",
+      lib_id: "script#0005",
+      haggadah_short_desc: "Different Family Script",
+      room_code: "AAAAAE",
+      path: "madliberation-scripts/005-Family_Script",
       is_script: 1,
-      haggadah_name: '0005 - Family Script',
-      script_number: 5
+      haggadah_name: "0005 - Family Script",
+      script_number: 5,
     },
     {
       haggadah_description:
-        'An offensive script for a different part of the family',
-      lib_id: 'script#0007',
-      haggadah_short_desc: 'Dirty Script',
-      room_code: 'AAAAAF',
-      path: 'madliberation-scripts/007-Dirty_Script',
+        "An offensive script for a different part of the family",
+      lib_id: "script#0007",
+      haggadah_short_desc: "Dirty Script",
+      room_code: "AAAAAF",
+      path: "madliberation-scripts/007-Dirty_Script",
       is_script: 1,
-      haggadah_name: '0007 - Dirty Script',
-      script_number: 7
+      haggadah_name: "0007 - Dirty Script",
+      script_number: 7,
     },
     {
       haggadah_description:
-        'An offensive script for the whole different family',
-      lib_id: 'script#0008',
-      haggadah_short_desc: 'Dirty Family Script',
-      room_code: 'AAAAAG',
-      path: 'madliberation-scripts/008-Dirty_Family_Script',
+        "An offensive script for the whole different family",
+      lib_id: "script#0008",
+      haggadah_short_desc: "Dirty Family Script",
+      room_code: "AAAAAG",
+      path: "madliberation-scripts/008-Dirty_Family_Script",
       is_script: 1,
-      haggadah_name: '0008 - Dirty Family Script',
-      script_number: 8
+      haggadah_name: "0008 - Dirty Family Script",
+      script_number: 8,
     },
     {
-      haggadah_description: 'A script to help you get good at Mad Liberation',
-      lib_id: 'script#0006',
-      haggadah_short_desc: 'Practice Script',
-      room_code: 'AAAAAH',
-      path: 'madliberation-scripts/006-Practice_Script',
+      haggadah_description: "A script to help you get good at Mad Liberation",
+      lib_id: "script#0006",
+      haggadah_short_desc: "Practice Script",
+      room_code: "AAAAAH",
+      path: "madliberation-scripts/006-Practice_Script",
       is_script: 1,
-      haggadah_name: '0006 - Practice Script',
-      script_number: 6
-    }
+      haggadah_name: "0006 - Practice Script",
+      script_number: 6,
+    },
   ];
 }
 function getProps({ scripts }) {
   return {
     scripts,
-    setChosenPath: jest.fn()
+    setChosenPath: jest.fn(),
   };
 }
 function expectedTable(scripts, selectedIndex) {
@@ -208,19 +210,32 @@ function expectedTable(scripts, selectedIndex) {
   );
 }
 
-describe('<ScriptTable />', () => {
-  test('scripts in props should appear in a table 1', () => {
+describe("<ScriptTable />", () => {
+  test("scripts in props should appear in a table 1", () => {
     const props = getProps({ scripts: fourScripts() });
-    const wrapper = mount(
+
+    render(
       <MemoryRouter>
-        <ScriptTable {...props} />
+        <ScriptTable {...props}></ScriptTable>
       </MemoryRouter>
     );
-    expect(
-      wrapper.containsMatchingElement(expectedTable(fourScripts(), 0))
-    ).toBe(true);
+    expect(screen.queryAllByText("Dirty Script")).toHaveLength(1);
+    expect(screen.getAllByRole("radio").length).toEqual(4);
+    expect(screen.getByText("Dirty Script")).toHaveTextContent("Dirty Script");
+    expect(screen.getAllByLabelText("Dirty Script").length).toEqual(1);
+    expect(screen.getByLabelText("Dirty Script")).toHaveAttribute(
+      "type",
+      "radio"
+    );
+    fourScripts().forEach((script) => {
+      expect(
+        screen.getByLabelText(
+          new RegExp(`^${script.haggadah_short_desc}$`)
+        )
+      ).toHaveAttribute("type", "radio");
+    });
   });
-  test('scripts in props should appear in a table 2', () => {
+  test("scripts in props should appear in a table 2", () => {
     const props = getProps({ scripts: differentScripts() });
     const wrapper = mount(
       <MemoryRouter>
@@ -232,8 +247,8 @@ describe('<ScriptTable />', () => {
     ).toBe(true);
   });
   test(
-    'the Use This One button should call setChosenPath with the selected' +
-      ' script, after others have been clicked',
+    "the Use This One button should call setChosenPath with the selected" +
+      " script, after others have been clicked",
     () => {
       const props = getProps({ scripts: fourScripts() });
       const wrapper = mount(
@@ -254,21 +269,21 @@ describe('<ScriptTable />', () => {
       // submit
       wrapper
         .findWhere(
-          n =>
-            n.is(Button) && n.is({ madliberationid: 'pick-this-script-button' })
+          (n) =>
+            n.is(Button) && n.is({ madliberationid: "pick-this-script-button" })
         )
-        .prop('onClick')();
+        .prop("onClick")();
       // verify setChosenPath was called once with the 2nd script
       expect(props.setChosenPath).toHaveBeenCalled();
       expect(props.setChosenPath).toHaveBeenCalledTimes(1);
       expect(props.setChosenPath).toHaveBeenCalledWith(
-        'madliberation-scripts/002-Dirty_Script'
+        "madliberation-scripts/002-Dirty_Script"
       );
     }
   );
   test(
-    'the Use This One button should call setChosenPath with the selected' +
-      ' script 2',
+    "the Use This One button should call setChosenPath with the selected" +
+      " script 2",
     () => {
       const props = getProps({ scripts: differentScripts() });
       const wrapper = mount(
@@ -278,19 +293,19 @@ describe('<ScriptTable />', () => {
       );
       wrapper
         .findWhere(
-          n =>
-            n.is(Button) && n.is({ madliberationid: 'pick-this-script-button' })
+          (n) =>
+            n.is(Button) && n.is({ madliberationid: "pick-this-script-button" })
         )
-        .prop('onClick')();
+        .prop("onClick")();
       // verify setChosenPath was called once with the 1st script
       expect(props.setChosenPath).toHaveBeenCalled();
       expect(props.setChosenPath).toHaveBeenCalledTimes(1);
       expect(props.setChosenPath).toHaveBeenCalledWith(
-        'madliberation-scripts/005-Family_Script'
+        "madliberation-scripts/005-Family_Script"
       );
     }
   );
-  test('The Pick This One button should be a link to /generating-room-code', () => {
+  test("The Pick This One button should be a link to /generating-room-code", () => {
     const expectedButton = (
       <Button
         madliberationid="pick-this-script-button"
@@ -309,11 +324,11 @@ describe('<ScriptTable />', () => {
     );
     expect(wrapper.containsMatchingElement(expectedButton)).toBe(true);
   });
-  test('Rows, Cells, and Radios should have unique keys', () => {
+  test("Rows, Cells, and Radios should have unique keys", () => {
     // A failiure of this requirement would show up in the console, no need to
     // test for now
   });
-  test('Scripts should be ordered by lib_id', () => {
+  test("Scripts should be ordered by lib_id", () => {
     // This is unnecessary: the scripts are returned by the API sorted by their
     // sort key
   });

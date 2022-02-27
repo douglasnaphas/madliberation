d0bd38e037f05d8467208f8fb6b44d885f9f7c76
Douglas Naphas <douglasnaphas@gmail.com>
2022-02-26 21:42:46 -0500

    Rewrite ScriptTable tests with testing lib
    gh-388

--- frontend/src/components/ScriptTable.test.js
+++ frontend/src/components/ScriptTable.test.js
@@ -1,6 +1,3 @@
-import { Button } from "@material-ui/core";
-import { createMount } from "@material-ui/core/test-utils";
-import { Link } from "react-router-dom";
 import { MemoryRouter } from "react-router-dom";
 import React from "react";
 import Table from "@material-ui/core/Table";
@@ -9,79 +6,15 @@ import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import Radio from "@material-ui/core/Radio";
 import {
+  getAllByRole,
   getByLabelText,
   getByText,
   render,
   screen,
 } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
+import userEvent from "@testing-library/user-event";
 import ScriptTable from "./ScriptTable";
-import {
-  testUtils,
-  expectSelectorCount,
-  expectFewerThanNOfSelector,
-} from "../test/testUtils";
-
-let mount;
-beforeEach(() => {
-  mount = createMount();
-});
-afterEach(() => {
-  mount.cleanUp();
-});
-
-/**
- * Click the radio button next to script number scriptNumber
- * @param {ReactWrapper} wrapper The wrapper to search
- * @param {Number} scriptNumber The id of the script in the table
- */
-function selectScript(wrapper, scriptNumber) {
-  wrapper
-    .findWhere((n) => n.is(Radio) && n.is(`#script-${scriptNumber}`))
-    .prop("onChange")({
-    target: { value: `${scriptNumber}` },
-  });
-  wrapper.update();
-}
-
-/**
- * @returns {boolean} true if script number scriptNumber is present and
- * selected, false otherwise
- * @param {ReactWrapper} wrapper The wrapper to search
- * @param {Number} scriptNumber The id of the script in the table
- */
-function scriptChecked(wrapper, scriptNumber) {
-  return wrapper
-    .findWhere(
-      (n) =>
-        n.is(Radio) && n.is(`#script-${scriptNumber}`) && n.is("[checked=true]")
-    )
-    .exists();
-}
-
-/**
- * @returns {boolean} true if every script number in scriptNumbers is present
- * and unchecked, false otherwise
- * @param {ReactWrapper} wrapper
- * @param {Array[Number]} scriptNumbers
- */
-function scriptsUnchecked(wrapper, scriptNumbers) {}
-
-/**
- * @returns {boolean} true if every index in {0, 1, ..., scriptCount - 1} is
- * unchecked and selectedScriptNumber is checked, false otherwise
- * @param {ReactWrapper} wrapper
- * @param {Number} selectedScriptNumber
- * @param {Number} scriptCount
- */
-function scriptCheckedOnly(wrapper, selectedScriptNumber, scriptCount) {
-  if (!scriptChecked(wrapper, selectedScriptNumber)) return false;
-  for (let i = 0; i < scriptCount; i++) {
-    if (i != selectedScriptNumber && scriptChecked(i)) return false;
-  }
-  return true;
-}
 
 function fourScripts() {
   return [
@@ -180,40 +113,6 @@ function getProps({ scripts }) {
     setChosenPath: jest.fn(),
   };
 }
-function expectedTable(scripts, selectedIndex) {
-  return (
-    <Table>
-      <TableBody>
-        {scripts.map((script, index) => {
-          const scriptUid = `${script.room_code}-${script.lib_id}`;
-          return (
-            <TableRow key={`row${scriptUid}`}>
-              <TableCell key={`${scriptUid}-select`}>
-                <Radio
-                  key={`${scriptUid}-radio`}
-                  checked={`${selectedIndex}` === `${index}`}
-                  value={`${index}`}
-                  id={`script-${index}`}
-                />
-              </TableCell>
-              <TableCell key={`${scriptUid}-short_desc`}>
-                <label
-                  htmlFor={`script-${index}`}
-                  madliberationid={`${script.haggadah_short_desc}`}
-                >
-                  {script.haggadah_short_desc}
-                </label>
-              </TableCell>
-              <TableCell key={`${scriptUid}-description`}>
-                {script.haggadah_description}
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
-  );
-}
 
 describe("<ScriptTable />", () => {
   test("scripts in props should appear in a table 1", () => {
@@ -251,37 +150,26 @@ describe("<ScriptTable />", () => {
   test(
     "the Use This One button should call setChosenPath with the selected" +
       " script, after others have been clicked",
-    () => {
+    async () => {
       const props = getProps({ scripts: fourScripts() });
       render(
         <MemoryRouter>
           <ScriptTable {...props} />
         </MemoryRouter>
       );
-      const radioGroups = screen.getAllByRole("radiogroup")
+      const radioGroups = screen.getAllByRole("radiogroup");
       expect(radioGroups.length).toEqual(1);
       const radioGroup = radioGroups[0];
-      expect(screen.getByLabelText("Family Script")).toHaveAttribute("checked")
-      return;
-      // click the 3rd script
-      selectScript(wrapper, 2);
-      expect(scriptChecked(wrapper, 0)).toBe(false);
-      expect(scriptChecked(wrapper, 2)).toBe(true);
-      expectSelectorCount(wrapper, Radio, 4);
-      expect(scriptCheckedOnly(wrapper, 2)).toBe(true);
-      // click the 2nd script
-      selectScript(wrapper, 1);
-      expect(scriptCheckedOnly(wrapper, 1)).toBe(true);
-      // submit
-      wrapper
-        .findWhere(
-          (n) =>
-            n.is(Button) && n.is({ madliberationid: "pick-this-script-button" })
-        )
-        .prop("onClick")();
-      // verify setChosenPath was called once with the 2nd script
-      expect(props.setChosenPath).toHaveBeenCalled();
-      expect(props.setChosenPath).toHaveBeenCalledTimes(1);
+      const radios = screen.getAllByRole("radio");
+      const radiosInGroup = getAllByRole(radioGroup, "radio");
+      expect(radios.length).toEqual(radiosInGroup.length);
+      expect(getByLabelText(radioGroup, "Family Script")).toBeChecked();
+      await userEvent.click(screen.getByLabelText("Dirty Family Script"));
+      expect(getByLabelText(radioGroup, "Dirty Family Script")).toBeChecked();
+      await userEvent.click(screen.getByLabelText("Dirty Script"));
+      const button = screen.getByRole("button");
+      expect(button).toHaveTextContent("Use this one");
+      await userEvent.click(button);
       expect(props.setChosenPath).toHaveBeenCalledWith(
         "madliberation-scripts/002-Dirty_Script"
       );
@@ -290,52 +178,30 @@ describe("<ScriptTable />", () => {
   test(
     "the Use This One button should call setChosenPath with the selected" +
       " script 2",
-    () => {
+    async () => {
       const props = getProps({ scripts: differentScripts() });
-      const wrapper = mount(
+      render(
         <MemoryRouter>
           <ScriptTable {...props} />
         </MemoryRouter>
       );
-      wrapper
-        .findWhere(
-          (n) =>
-            n.is(Button) && n.is({ madliberationid: "pick-this-script-button" })
-        )
-        .prop("onClick")();
-      // verify setChosenPath was called once with the 1st script
-      expect(props.setChosenPath).toHaveBeenCalled();
-      expect(props.setChosenPath).toHaveBeenCalledTimes(1);
+      expect(screen.getByLabelText("Different Family Script")).toBeChecked();
+      await userEvent.click(screen.getByRole("button"));
       expect(props.setChosenPath).toHaveBeenCalledWith(
         "madliberation-scripts/005-Family_Script"
       );
     }
   );
   test("The Pick This One button should be a link to /generating-room-code", () => {
-    const expectedButton = (
-      <Button
-        madliberationid="pick-this-script-button"
-        variant="contained"
-        component={Link}
-        to="/generating-room-code"
-      >
-        Use this one
-      </Button>
-    );
     const props = getProps({ scripts: fourScripts() });
-    const wrapper = mount(
+    render(
       <MemoryRouter>
-        <ScriptTable {...props} />
+        <ScriptTable {...props}></ScriptTable>
       </MemoryRouter>
     );
-    expect(wrapper.containsMatchingElement(expectedButton)).toBe(true);
-  });
-  test("Rows, Cells, and Radios should have unique keys", () => {
-    // A failiure of this requirement would show up in the console, no need to
-    // test for now
-  });
-  test("Scripts should be ordered by lib_id", () => {
-    // This is unnecessary: the scripts are returned by the API sorted by their
-    // sort key
+    expect(screen.getByText("Use this one").closest("a")).toHaveAttribute(
+      "href",
+      "/generating-room-code"
+    );
   });
 });

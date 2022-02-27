1072c491c025693171bbe62e7fc24c0656e86862
Douglas Naphas <douglasnaphas@gmail.com>
2022-02-26 23:10:34 -0500

    Rewrite part of a SedersPage test in RTL
    gh-388
    
    (React Testing Library)

--- frontend/src/components/SedersPage.test.js
+++ frontend/src/components/SedersPage.test.js
@@ -5,6 +5,14 @@ import { MemoryRouter } from "react-router-dom";
 import { act } from "react-dom/test-utils";
 import Button from "@material-ui/core/Button";
 import Radio from "@material-ui/core/Radio";
+import {
+  getAllByRole,
+  getByLabelText,
+  getByText,
+  render,
+  screen,
+} from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 let mount;
 let globalFetch = global.fetch;
@@ -119,6 +127,104 @@ describe("SedersPage", () => {
         );
         button.prop("onClick")();
       });
+      expect(setConfirmedRoomCode).toHaveBeenCalled();
+      expect(setConfirmedRoomCode).toHaveBeenCalledTimes(1);
+      expect(setConfirmedRoomCode).toHaveBeenCalledWith("ZLSXQA");
+      expect(setChosenPath).toHaveBeenCalled();
+      expect(setChosenPath).toHaveBeenCalledTimes(1);
+      expect(setChosenPath).toHaveBeenCalledWith(
+        "madliberation-scripts/006-Practice_Script"
+      );
+      expect(history.push).toHaveBeenCalled();
+      expect(history.push).toHaveBeenCalledWith("/your-room-code");
+    });
+    test("user rejoins an open seder 1 RTL", async () => {
+      const user = {
+        email: "user1@gmail.com",
+        nickname: "Mister One",
+        sub: "11-aa-m1-gha-one",
+      };
+      const setConfirmedRoomCode = jest.fn();
+      const setChosenPath = jest.fn();
+
+      const sedersStarted = [
+        {
+          created: 1585970508141,
+          lib_id: "seder",
+          room_code: "IJYAQX",
+          path: "madliberation-scripts/005-Practice_Script",
+          user_email: "user1@gmail.com",
+          timestamp: "2020-04-04T03:21:48.141Z",
+        },
+        {
+          created: 1585973347633,
+          lib_id: "seder",
+          room_code: "ZLSXQA",
+          path: "madliberation-scripts/006-Practice_Script",
+          user_email: "user1@gmail.com",
+          timestamp: "2020-04-04T04:09:07.633Z",
+        },
+        {
+          created: 1585963851309,
+          lib_id: "seder",
+          room_code: "GMKMNB",
+          path: "madliberation-scripts/007-Practice_Script",
+          user_email: "user1@gmail.com",
+          timestamp: "2020-04-04T01:30:51.309Z",
+        },
+      ];
+      const sedersJoined = [];
+      global.fetch = jest.fn().mockImplementation((url, init) => {
+        if (
+          url.pathname === "/prod/seders" ||
+          url.pathname === "/seders-started"
+        ) {
+          return new Promise((resolve, reject) => {
+            resolve({
+              json: jest.fn().mockImplementation(() => {
+                return { Items: sedersStarted };
+              }),
+            });
+          });
+        }
+        if (url.pathname === "/prod/seders-joined") {
+          return new Promise((resolve, reject) => {
+            resolve({
+              json: jest.fn().mockImplementation(() => {
+                return { Items: sedersJoined };
+              }),
+            });
+          });
+        }
+      });
+      const history = { push: jest.fn() };
+      render(
+        <MemoryRouter>
+          <SedersPage
+            history={history}
+            user={user}
+            setConfirmedRoomCode={setConfirmedRoomCode}
+            setChosenPath={setChosenPath}
+          ></SedersPage>
+        </MemoryRouter>
+      );
+      expect(global.fetch).toHaveBeenCalled();
+      const resumeSederButton = (
+        await screen.findByText("Resume seder")
+      ).closest("button");
+      expect(resumeSederButton).toHaveTextContent("Resume seder");
+
+      // selectSederByRoomCode(wrapper, "ZLSXQA");
+      // wrapper.update();
+      // act(() => {
+      //   const button = wrapper.findWhere(
+      //     (n) => n.is(Button) && n.is("#resume-this-seder-button")
+      //   );
+      //   button.prop("onClick")();
+      // });
+
+      return;
+
       expect(setConfirmedRoomCode).toHaveBeenCalled();
       expect(setConfirmedRoomCode).toHaveBeenCalledTimes(1);
       expect(setConfirmedRoomCode).toHaveBeenCalledWith("ZLSXQA");

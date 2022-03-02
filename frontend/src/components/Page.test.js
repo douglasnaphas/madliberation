6e256c5b580670ab76d98e1a98450fc35ba86b7d
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-01 21:57:36 -0500

    Rewrite Page tests in testing-library
    gh-388

--- frontend/src/components/Page.test.js
+++ frontend/src/components/Page.test.js
@@ -1,119 +1,90 @@
-import { Button } from '@material-ui/core';
-import { createMount } from '@material-ui/core/test-utils';
-import Page from './Page';
-import React from 'react';
+import Page from "./Page";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
-describe('<Page />', () => {
-  let mount;
-
-  beforeEach(() => {
-    mount = createMount();
-  });
-
-  afterEach(() => {
-    mount.cleanUp();
-  });
-
+describe("<Page />", () => {
   const p1 = {
     lines: [
       {
-        type: 'h1',
-        segments: [{ type: 'text', text: 'A Very Practice Passover' }]
+        type: "h1",
+        segments: [{ type: "text", text: "A Very Practice Passover" }],
       },
       {
-        type: 'h4',
+        type: "h4",
         segments: [
           {
-            type: 'text',
-            text: 'A safe space to learn how to play Mad Liberation'
-          }
-        ]
+            type: "text",
+            text: "A safe space to learn how to play Mad Liberation",
+          },
+        ],
       },
       {
-        type: 'p',
+        type: "p",
         segments: [
-          { type: 'text', text: 'Come, Jew, on a Jewney of liberation.' }
-        ]
+          { type: "text", text: "Come, Jew, on a Jewney of liberation." },
+        ],
       },
       {
-        type: 'h2',
-        segments: [{ type: 'text', text: 'The bad old days' }]
+        type: "h2",
+        segments: [{ type: "text", text: "The bad old days" }],
       },
       {
-        type: 'p',
-        segments: [
-          {
-            type: 'text',
-            text:
-              'Have you ever been to a seder where someone says, “Stop looking at your phone”?'
-          }
-        ]
-      },
-      {
-        type: 'p',
+        type: "p",
         segments: [
           {
-            type: 'text',
-            text:
-              'The only thing that ruins Passover quicker than that is when someone '
+            type: "text",
+            text: "Have you ever been to a seder where someone says, “Stop looking at your phone”?",
+          },
+        ],
+      },
+      {
+        type: "p",
+        segments: [
+          {
+            type: "text",
+            text: "The only thing that ruins Passover quicker than that is when someone ",
           },
           {
-            type: 'lib',
+            type: "lib",
             id: 1,
-            prompt: 'does something disruptive',
-            answer: 'one'
+            prompt: "does something disruptive",
+            answer: "one",
           },
-          { type: 'text', text: '.' }
-        ]
+          { type: "text", text: "." },
+        ],
       },
       {
-        type: 'p',
+        type: "p",
         segments: [
           {
-            type: 'text',
-            text: 'Here is another lib, coming at you like '
+            type: "text",
+            text: "Here is another lib, coming at you like ",
           },
           {
-            type: 'lib',
+            type: "lib",
             id: 2,
-            prompt: 'something fast and heavy',
-            answer: 'two'
+            prompt: "something fast and heavy",
+            answer: "two",
           },
-          { type: 'text', text: '.' }
-        ]
-      }
-    ]
+          { type: "text", text: "." },
+        ],
+      },
+    ],
   };
-  const readyToReadButton = (
-    <Button
-      madliberationid="ready-to-read-button"
-      variant="contained"
-      color="primary"
-    >
-      Ready to read
-    </Button>
-  );
-  const nextPageButton = (
-    <Button
-      madliberationid="next-page-button"
-      variant="contained"
-      color="primary"
-    >
-      Next page
-    </Button>
-  );
 
-  test('full DOM rendering', async () => {
-    const wrapper = await mount(<Page page={p1} pageIndex={0} />);
-    wrapper.update();
-    expect(wrapper.containsMatchingElement(readyToReadButton)).toBeTruthy();
-    expect(wrapper.containsMatchingElement(nextPageButton)).toBeFalsy();
-    wrapper
-      .find('[madliberationid="ready-to-read-button"]')
-      .first()
-      .simulate('click');
-    expect(wrapper.containsMatchingElement(readyToReadButton)).toBeFalsy();
-    expect(wrapper.containsMatchingElement(nextPageButton)).toBeTruthy();
-    wrapper.unmount();
+  test("full DOM rendering", async () => {
+    render(<Page page={p1} pageIndex={0} />);
+    const readyToReadButton = screen
+      .getByText("Ready to read")
+      .closest("button");
+    expect(readyToReadButton).toHaveTextContent("Ready to read");
+    expect(screen.queryByText("Next page")).toBeNull();
+    await userEvent.click(readyToReadButton);
+    expect(screen.queryByText("Ready to read")).toBeNull();
+    expect(screen.getByText("Next page").closest("button")).toHaveTextContent(
+      "Next page"
+    );
   });
 });

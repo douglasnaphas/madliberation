7734cc4d0292242e544de5f493aaa46f9ebaa646
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-13 18:33:27 -0400

    Consider email when flagging authed requests
    gh-253

--- backend/lib/flagAuthedRequests.js
+++ backend/lib/flagAuthedRequests.js
@@ -2,29 +2,39 @@ const flagAuthedRequests = () => {
   const middleware = (req, res, next) => {
     const logger = require("../logger");
     const responses = require("../responses");
-    if(req.method === 'POST') {
-      if(!req.body || !req.body.user) return next();
-      if(typeof req.body.user !== 'string') {
-        logger.log(`flagAuthedRequests: non-string sub`);
-        return res.status(400).send(responses.BAD_REQUEST);
+    if (req.method === "POST") {
+      if (!req.body) {
+        return next();
       }
-      res.locals.user = req.body.user;
+      if (!req.body.user && !req.body.email) {
+        return next();
+      }
+      if (req.body.user) {
+        res.locals.user = req.body.user;
+      }
+      if (req.body.email) {
+        res.locals.preAuthEmail = decodeURIComponent(req.body.email);
+      }
       return next();
-    } else if(req.method === 'GET') {
-      if(!req.query || !req.query.user) return next();
-      if(typeof req.query.user !== 'string') {
-        logger.log(`flagAuthedRequests: non-string sub`);
-        return res.status(400).send(responses.BAD_REQUEST);
+    } else if (req.method === "GET") {
+      if (!req.query) {
+        return next();
       }
-      res.locals.user = req.query.user;
+      if (!req.query.user && !req.query.email) {
+        return next();
+      }
+      if (req.query.user) {
+        res.locals.user = req.query.user;
+      }
+      if (req.query.email) {
+        res.locals.preAuthEmail = decodeURIComponent(req.query.email);
+      }
       return next();
     }
-    logger.log(
-      {
-        message: `flagAuthedRequests: non-POST, non-GET`,
-        method: req.method
-      }
-    );
+    logger.log({
+      message: `flagAuthedRequests: non-POST, non-GET`,
+      method: req.method,
+    });
     return next();
   };
   return middleware;
--- backend/lib/flagAuthedRequests.test.js
+++ backend/lib/flagAuthedRequests.test.js
@@ -1,125 +1,143 @@
 /* globals expect, jest */
-describe('lib/flagAuthedRequests', () => {
-  const flagAuthedRequests = require('./flagAuthedRequests');
-  const responses = require('../responses');
-  describe('GET requests', () => {
-    test('no user param, expect next', () => {
-      const req = {method: 'GET', query: {}};
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
+describe("lib/flagAuthedRequests", () => {
+  const flagAuthedRequests = require("./flagAuthedRequests");
+  const responses = require("../responses");
+  test.each([
+    {
+      description: "GET, user, no email 1",
+      req: {
+        method: "GET",
+        query: { user: "afjda0-943fakd8-fajsdk23-ffasdf3" },
+      },
+      expectNext: true,
+      expectedLocals: { user: "afjda0-943fakd8-fajsdk23-ffasdf3" },
+    },
+    {
+      description: "GET, user, no email 2",
+      req: {
+        method: "GET",
+        query: { user: "fjsadkl-4324-fa3erewio" },
+      },
+      expectNext: true,
+      expectedLocals: { user: "fjsadkl-4324-fa3erewio" },
+    },
+    {
+      description: "GET, user, email 1",
+      req: {
+        method: "GET",
+        query: {
+          user: "fjsadkl-4324-fa3erewio",
+          email: encodeURIComponent("me@you.com"),
+        },
+      },
+      expectNext: true,
+      expectedLocals: {
+        user: "fjsadkl-4324-fa3erewio",
+        preAuthEmail: "me@you.com",
+      },
+    },
+    {
+      description: "GET, no user, email 1",
+      req: {
+        method: "GET",
+        query: {
+          email: encodeURIComponent("thing@theother.com"),
+        },
+      },
+      expectNext: true,
+      expectedLocals: {
+        preAuthEmail: "thing@theother.com",
+      },
+    },
+    {
+      description: "GET, no user, no email",
+      req: {
+        method: "GET",
+        body: {},
+      },
+      expectNext: true,
+      expectedLocals: {},
+    },
+    {
+      description: "GET, no body",
+      req: {
+        method: "GET",
+      },
+      expectNext: true,
+      expectedLocals: {},
+    },
+    {
+      description: "POST, user, no email 1",
+      req: {
+        method: "POST",
+        body: { user: "afjda0-943fakd8-fajsdk23-ffasdf3" },
+      },
+      expectNext: true,
+      expectedLocals: { user: "afjda0-943fakd8-fajsdk23-ffasdf3" },
+    },
+    {
+      description: "POST, user, no email 2",
+      req: {
+        method: "POST",
+        body: { user: "fjsadkl-4324-fa3erewio" },
+      },
+      expectNext: true,
+      expectedLocals: { user: "fjsadkl-4324-fa3erewio" },
+    },
+    {
+      description: "POST, user, email 1",
+      req: {
+        method: "POST",
+        body: {
+          user: "fjsadkl-4324-fa3erewio",
+          email: encodeURIComponent("me@you.com"),
+        },
+      },
+      expectNext: true,
+      expectedLocals: {
+        user: "fjsadkl-4324-fa3erewio",
+        preAuthEmail: "me@you.com",
+      },
+    },
+    {
+      description: "POST, no user, email 1",
+      req: {
+        method: "POST",
+        body: {
+          email: encodeURIComponent("me@you.com"),
+        },
+      },
+      expectNext: true,
+      expectedLocals: {
+        preAuthEmail: "me@you.com",
+      },
+    },
+    {
+      description: "POST, no user, no email",
+      req: {
+        method: "POST",
+        body: {},
+      },
+      expectNext: true,
+      expectedLocals: {},
+    },
+    {
+      description: "POST, no body",
+      req: {
+        method: "POST",
+      },
+      expectNext: true,
+      expectedLocals: {},
+    },
+  ])("$description", ({ req, expectNext, expectedLocals }) => {
+    const res = { locals: {}, send: jest.fn() };
+    res.status = jest.fn(() => res);
+    const next = jest.fn();
+    const middleware = flagAuthedRequests();
+    middleware(req, res, next);
+    if (expectNext) {
       expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toBeUndefined();
-    });
-    test('user sub provided 1', () => {
-      const req = {
-        method: 'GET',
-        query: {user: 'afjda0-943fakd8-fajsdk23-ffasdf3'}
-      };
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toEqual(req.query.user);
-    });
-    test('user sub provided 2', () => {
-      const req = {
-        method: 'GET',
-        query: {user: 'fjsadkl-4324-fa3erewio'}
-      };
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toEqual(req.query.user);
-    });
-    test('weird sub provided, should be accepted here', () => {
-      const req = {
-        method: 'GET',
-        query: {user: 'even odd subs are accepted, they just have to' +
-        ' be strings'
-      }};
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toEqual(req.query.user);
-    });
-  });
-  describe('POST requests', () => {
-    test('no user or email param, expect next', () => {
-      const req = {method: 'POST', body: {}};
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toBeUndefined();
-    });
-    test('email provided 1', () => {
-      const req = {
-        method: 'POST',
-        body: {user: 'afjda0-943fakd8-fajsdk23-ffasdf3'}
-      };
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toEqual(req.body.user);
-    });
-    test('email provided 2', () => {
-      const req = {
-        method: 'POST',
-        body: {user: 'fjsadkl-4324-fa3erewio'}
-      };
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toEqual(req.body.user);
-    });
-    test('bad email provided, should be accepted here', () => {
-      const req = {
-        method: 'POST',
-        body: {user: 'even odd subs are accepted, they just have to' +
-        ' be strings'
-      }};
-      const send = jest.fn();
-      const res = {locals: {}, status: jest.fn(() => ({send}))};
-      const next = jest.fn();
-      const middleware = flagAuthedRequests();
-      middleware(req, res, next);
-      expect(next).toHaveBeenCalled();
-      expect(send).not.toHaveBeenCalled();
-      expect(res.status).not.toHaveBeenCalled();
-      expect(res.locals.user).toEqual(req.body.user);
-    });
+    }
+    expect(res.locals).toEqual(expectedLocals);
   });
 });

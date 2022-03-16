d403f2d50d6054f7f7ebe535ff4cc387ae199566
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-15 21:17:03 -0400

    Shift to authentication with the opaque cookie
    gh-253

--- backend/lib/authenticateWithOpaqueCookie.js
+++ backend/lib/authenticateWithOpaqueCookie.js
@@ -1,34 +1,56 @@
 const flagAuthedRequests = require("./flagAuthedRequests");
-const pullTokensFromCookies = require("./pullTokensFromCookies");
-const getJwksConditionally = require("./getJwksConditionally");
-const getCognitoClientSecret = require("./getCognitoClientSecret");
-const awsSdk = require("aws-sdk");
-const setDbParamsGetEmailFromSub = require("./setDbParamsGetEmailFromSub");
-const runQuery = require("./runQuery");
 const validateOpaqueCookie = require("./validateOpaqueCookie");
+const runGet = require("./runGet");
+const logger = require("../logger");
+const schema = require("../schema");
+const dbParamsGetEmailFromOpaqueCookie = require("./dbParamsGetEmailFromOpaqueCookie")
 
 const authenticate = [
-  // saved in res.locals:
   // get the preAuthEmail from the various places it could be (req.query.email,
   // req.body.email for POSTs), decode it from URI encoding, and save it in
   // res.locals.preAuthEmail
-  flagAuthedRequests(),
+  flagAuthedRequests(), // preAuthEmail
   ///////// below here only happens if this is an authenticated request ////////
   ///////// but middlewares have to enforce that rule individually /////////////
   ////////// by looking for res.locals.preAuthEmail ////////////////////////////
-
-  validateOpaqueCookie({ local: "preAuthEmail" }),
-
-  // dbParamsGetEmailFromOpaqueCookie("preAuthEmail")
-
-
-  // runQuery
-
+  validateOpaqueCookie({ local: "preAuthEmail" }), // loginCookie
+  dbParamsGetEmailFromOpaqueCookie("preAuthEmail"), // dbParamsGetEmailFromOpaqueCookie
+  runGet("preAuthEmail"), // dbError, dbData
   // fail if error
-
-  // compare email to req.email
-
+  (req, res, next) => {
+    if (res.locals.dbError) {
+      logger.log("dbError getting email from opaque cookie");
+      logger.log(dbError);
+      return res.sendStatus(500);
+    }
+    return next();
+  },
+  // compare email to res.locals.preAuthEmail
   // set res.locals.userEmail
+  (req, res, next) => {
+    if (!res.locals.preAuthEmail) {
+      return next();
+    }
+    if (
+      !res.locals.dbData ||
+      !res.locals.dbData.Item ||
+      !res.locals.dbData.Item[schema.USER_EMAIL]
+    ) {
+      logger.log(
+        `email not found in db for user claiming to be ${res.locals.preAuthEmail}`
+      );
+      return res.sendStatus(400);
+    }
+    const dbEmail = res.locals.dbData.Item[schema.USER_EMAIL];
+    if (res.locals.preAuthEmail !== dbEmail) {
+      logger.log(
+        `user claimed to be ${res.locals.preAuthEmail},` +
+          ` cookie said ${dbEmail}`
+      );
+      return res.sendStatus(400);
+    }
+    res.locals.userEmail = dbEmail;
+  }, // userEmail
 
   //////////////////////////////////////////////////////////////////////////////
   // the end goal of this whole series is to populate res.locals.userEmail    //
--- backend/lib/setLoginCookie.js
+++ backend/lib/setLoginCookie.js
@@ -1,13 +1,14 @@
 const Configs = require("../Configs");
 const setLoginCookie = () => {
   const middleware = (req, res, next) => {
-    if (!res.locals.opaqueCookie) {
+    if (!res.locals.opaqueCookie || !res.locals.opaqueCookieExpirationMs) {
       return res.status(500).send("problem generating login cookie");
     }
     const cookieOptions = {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
+      expires: new Date(res.locals.opaqueCookieExpirationMs),
     };
     res.cookie(
       Configs.loginCookieName(),

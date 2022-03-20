5be0b2963ac8ae946dd1eb6de3d019fe58292a5f
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-19 22:14:54 -0400

    Log WS_ENDPOINT in initialization and handler
    gh-250

--- backend/trigger.js
+++ backend/trigger.js
@@ -4,8 +4,14 @@ const db = new DynamoDB.DocumentClient();
 const api = new ApiGatewayManagementApi({
   endpoint: process.env.WS_ENDPOINT,
 });
+const { WS_ENDPOINT } = process.env;
 
 exports.handler = async function (event, context, callback) {
+  console.log(
+    "process.env.WS_ENDPOINT during initialization, then in handler:"
+  );
+  console.log(WS_ENDPOINT);
+  console.log(process.env.WS_ENDPOINT);
   let connections;
   console.log("event:");
   console.log(event);

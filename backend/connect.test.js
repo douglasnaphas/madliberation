325e5b0d8ede8b41a00e34e982e76668b4013972
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-25 19:33:53 -0400

    Test some more cases, connect.js
    gh-250

--- backend/connect.test.js
+++ backend/connect.test.js
@@ -1,7 +1,11 @@
 const DynamoDB = require("aws-sdk/clients/dynamodb");
 
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.resetModules();
+});
 describe("connect", () => {
-  test("happy path", async () => {
+  test("happy path 0", async () => {
     const event = {
       headers: {
         "Cache-Control": "no-cache",
@@ -76,7 +80,6 @@ describe("connect", () => {
     const mockGet = jest.fn(() => {
       return {
         promise() {
-          console.log('in promise...')
           const data = {
             Item: {
               lib_id:
@@ -86,27 +89,17 @@ describe("connect", () => {
               game_name: "Le",
             },
           };
-          return data;
-          Promise.resolve({
-            Item: {
-              lib_id:
-                "participant#a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89",
-              room_code: "ZSMORM",
-              session_key: "VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
-              game_name: "Le",
-            },
-          });
+          return Promise.resolve(data);
         },
       };
     });
     const mockPut = jest.fn(() => {
       return {
         promise() {
-          Promise.resolve({});
+          return Promise.resolve({});
         },
       };
     });
-
     jest.mock("aws-sdk/clients/dynamodb", () => {
       return {
         DocumentClient: jest.fn(() => {
@@ -119,6 +112,181 @@ describe("connect", () => {
     });
     const handler = require("./connect").handler;
     const result = await handler(event);
-    console.log(result);
+    expect(result.statusCode).toEqual(200);
+  });
+  test.each([
+    {
+      description: "happy path 1",
+      event: {
+        headers: {
+          Cookie:
+            "a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89=VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+        },
+        multiValueHeaders: {
+          Cookie: [
+            "a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89=VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+          ],
+        },
+        queryStringParameters: { gamename: "Le", roomcode: "ZSMORM" },
+        multiValueQueryStringParameters: {
+          gamename: ["Le"],
+          roomcode: ["ZSMORM"],
+        },
+        requestContext: {
+          connectionId: "PeDU-fl2oAMCJfQ=",
+        },
+      },
+      Item: {
+        lib_id:
+          "participant#a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89",
+        room_code: "ZSMORM",
+        session_key: "VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+        game_name: "Le",
+      },
+      expectedStatus: 200,
+    },
+    {
+      description: "happy path 2",
+      event: {
+        headers: {
+          Cookie: `${(
+            "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+            "D90B5184B3C30FC"
+          ).toLowerCase()}=SOMESESSIONKEYABCXYZ`,
+        },
+        multiValueHeaders: {
+          Cookie: [
+            `${(
+              "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+              "D90B5184B3C30FC"
+            ).toLowerCase()}=SOMESESSIONKEYABCXYZ`,
+          ],
+        },
+        queryStringParameters: {
+          gamename: "some%20string",
+          roomcode: "XSMORM",
+        },
+        multiValueQueryStringParameters: {
+          gamename: ["some%20string"],
+          roomcode: ["XSMORM"],
+        },
+        requestContext: {
+          connectionId: "PeDU-fl2oAMCJfQ=",
+        },
+      },
+      Item: {
+        lib_id: `participant#${(
+          "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+          "D90B5184B3C30FC"
+        ).toLowerCase()}`,
+        room_code: "XSMORM",
+        session_key: "SOMESESSIONKEYABCXYZ",
+        game_name: "some%20string",
+      },
+      expectedStatus: 200,
+    },
+    {
+      description: "Item not found",
+      event: {
+        headers: {
+          Cookie: `${(
+            "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+            "D90B5184B3C30FC"
+          ).toLowerCase()}=SOMESESSIONKEYABCXYZ`,
+        },
+        multiValueHeaders: {
+          Cookie: [
+            `${(
+              "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+              "D90B5184B3C30FC"
+            ).toLowerCase()}=SOMESESSIONKEYABCXYZ`,
+          ],
+        },
+        queryStringParameters: {
+          gamename: "some%20string",
+          roomcode: "XSMORM",
+        },
+        multiValueQueryStringParameters: {
+          gamename: ["some%20string"],
+          roomcode: ["XSMORM"],
+        },
+        requestContext: {
+          connectionId: "PeDU-fl2oAMCJfQ=",
+        },
+      },
+      Item: {},
+      expectedStatus: 400,
+    },
+    {
+      description: "Session key mismatch",
+      event: {
+        headers: {
+          Cookie: `${(
+            "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+            "D90B5184B3C30FC"
+          ).toLowerCase()}=WRONGSESSIONKEYABCXYZ`,
+        },
+        multiValueHeaders: {
+          Cookie: [
+            `${(
+              "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+              "D90B5184B3C30FC"
+            ).toLowerCase()}=WRONGSESSIONKEYABCXYZ`,
+          ],
+        },
+        queryStringParameters: {
+          gamename: "some%20string",
+          roomcode: "XSMORM",
+        },
+        multiValueQueryStringParameters: {
+          gamename: ["some%20string"],
+          roomcode: ["XSMORM"],
+        },
+        requestContext: {
+          connectionId: "PeDU-fl2oAMCJfQ=",
+        },
+      },
+      Item: {
+        lib_id: `participant#${(
+          "61D034473102D7DAC305902770471FD50F4C5B26F6831A56D" +
+          "D90B5184B3C30FC"
+        ).toLowerCase()}`,
+        room_code: "XSMORM",
+        session_key: "SOMESESSIONKEYABCXYZ",
+        game_name: "some%20string",
+      },
+      expectedStatus: 400,
+    },
+  ])("$description", async ({ event, Item, expectedStatus }) => {
+    const mockGet = jest.fn(() => {
+      return {
+        promise() {
+          const data = {
+            Item,
+          };
+          return Promise.resolve(data);
+        },
+      };
+    });
+    const mockPut = jest.fn(() => {
+      return {
+        promise() {
+          return Promise.resolve({});
+        },
+      };
+    });
+    jest.mock("aws-sdk/clients/dynamodb", () => {
+      return {
+        DocumentClient: jest.fn(() => {
+          return {
+            get: mockGet,
+            put: mockPut,
+          };
+        }),
+      };
+    });
+    const handler = require("./connect").handler;
+    const result = await handler(event);
+    expect(result.statusCode).toEqual(expectedStatus);
   });
 });

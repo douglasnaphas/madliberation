9d8e70480f212094d035fcc52561cc728ae3fb95
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-24 23:09:45 -0400

    Add a placeholder test for connect.js
    gh-250
    
    This test is mostly a mess. It doesn't succeed or make much sense in its
    attempt to mock the AWS SDK, and it breaks when I switch the AWS SDK
    from require to import.

---
+++ backend/connect.test.js
@@ -0,0 +1,133 @@
+beforeEach(() => {
+  jest.resetModules();
+});
+
+describe("connect handler", () => {
+  test("call handler", () => {
+    const connect = require("./connect");
+    const { handler } = connect;
+    handler();
+  });
+
+  test.skip("happy path", () => {
+    // This doesn't work. I don't know how to mock the AWS SDK.
+    const event = {
+      headers: {
+        "Cache-Control": "no-cache",
+        Cookie:
+          "a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89=VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+        Host: "280igs74mf.execute-api.us-east-1.amazonaws.com",
+        Origin: "https://d2onsx64yqssek.cloudfront.net",
+        Pragma: "no-cache",
+        "Sec-WebSocket-Extensions":
+          "permessage-deflate; client_max_window_bits",
+        "Sec-WebSocket-Key": "o+IA59iIQeFcp2gh4Vf3hQ==",
+        "Sec-WebSocket-Version": "13",
+        "User-Agent": "Amazon CloudFront",
+        Via: "1.1 15cde442051269a0307a638d23683c8c.cloudfront.net (CloudFront)",
+        "X-Amz-Cf-Id":
+          "n864a57OXe_IQ_uKXpV1GtbZUtEYnPdwPYJkBAyq3g6yCqhYC3PEhg==",
+        "X-Amzn-Trace-Id": "Root=1-623be5b9-5aa411431c907e6d43289f60",
+        "X-Forwarded-For": "20.124.98.204, 64.252.151.135",
+        "X-Forwarded-Port": "443",
+        "X-Forwarded-Proto": "https",
+      },
+      multiValueHeaders: {
+        "Cache-Control": ["no-cache"],
+        Cookie: [
+          "a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89=VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+        ],
+        Host: ["280igs74mf.execute-api.us-east-1.amazonaws.com"],
+        Origin: ["https://d2onsx64yqssek.cloudfront.net"],
+        Pragma: ["no-cache"],
+        "Sec-WebSocket-Extensions": [
+          "permessage-deflate; client_max_window_bits",
+        ],
+        "Sec-WebSocket-Key": ["o+IA59iIQeFcp2gh4Vf3hQ=="],
+        "Sec-WebSocket-Version": ["13"],
+        "User-Agent": ["Amazon CloudFront"],
+        Via: [
+          "1.1 15cde442051269a0307a638d23683c8c.cloudfront.net (CloudFront)",
+        ],
+        "X-Amz-Cf-Id": [
+          "n864a57OXe_IQ_uKXpV1GtbZUtEYnPdwPYJkBAyq3g6yCqhYC3PEhg==",
+        ],
+        "X-Amzn-Trace-Id": ["Root=1-623be5b9-5aa411431c907e6d43289f60"],
+        "X-Forwarded-For": ["20.124.98.204, 64.252.151.135"],
+        "X-Forwarded-Port": ["443"],
+        "X-Forwarded-Proto": ["https"],
+      },
+      queryStringParameters: { gamename: "Le", roomcode: "ZSMORM" },
+      multiValueQueryStringParameters: {
+        gamename: ["Le"],
+        roomcode: ["ZSMORM"],
+      },
+      requestContext: {
+        routeKey: "$connect",
+        eventType: "CONNECT",
+        extendedRequestId: "PeDU-HGxIAMFr5Q=",
+        requestTime: "24/Mar/2022:03:30:01 +0000",
+        messageDirection: "IN",
+        stage: "ws",
+        connectedAt: 1648092601239,
+        requestTimeEpoch: 1648092601241,
+        identity: {
+          userAgent: "Amazon CloudFront",
+          sourceIp: "64.252.151.135",
+        },
+        requestId: "PeDU-HGxIAMFr5Q=",
+        domainName: "280igs74mf.execute-api.us-east-1.amazonaws.com",
+        connectionId: "PeDU-fl2oAMCJfQ=",
+        apiId: "280igs74mf",
+      },
+      isBase64Encoded: false,
+    };
+    const mockGet = jest.fn(() => {
+      return {
+        promise: () => {
+          return {
+            Item: {
+              lib_id:
+                "participant#a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89",
+              room_code: "ZSMORM",
+              session_key: "VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+              game_name: "Le",
+            },
+          };
+        },
+      };
+    });
+    const mockPut = jest.fn(() => {
+      return {
+        promise: () => {
+          return {
+            Item: {
+              lib_id:
+                "participant#a2fbc754b9ee1dc5a793eeb2c804c5e6cf962f680909050f28a69b6cdfdbab89",
+              room_code: "ZSMORM",
+              session_key: "VMSSHYCHBHJRUGHOMAIHNKTRECLNAL",
+              game_name: "Le",
+            },
+          };
+        },
+      };
+    });
+    class MockDocumentClient {
+      constructor() {
+        return {
+          get: mockGet,
+          put: mockPut,
+        };
+      }
+    }
+    jest.doMock("aws-sdk/clients/dynamodb", () => {
+      return {
+        DocumentClient: MockDocumentClient,
+      };
+    });
+
+    const connect = require("./connect");
+    const { handler } = connect;
+    handler(event);
+  });
+});

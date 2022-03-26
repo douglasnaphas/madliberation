3110e6553b6d942ff085d86206066a4bcdf83f9b
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-26 17:45:14 -0400

    Use logger in stream.js
    gh-250

--- backend/stream.js
+++ backend/stream.js
@@ -2,6 +2,7 @@
  * Let participants know when new participants join the seder.
  */
 const schema = require("./schema");
+const logger = require("./logger")
 const ApiGatewayManagementApi = require("aws-sdk/clients/apigatewaymanagementapi");
 const DynamoDB = require("aws-sdk/clients/dynamodb");
 const db = new DynamoDB.DocumentClient();
@@ -11,49 +12,51 @@ const api = new ApiGatewayManagementApi({
 
 const isJoin = (record) => {
   if (!record.dynamodb) {
-    console.log("no record.dynamodb");
+    logger.log("no record.dynamodb");
     return false;
   }
   if (record.dynamodb.OldImage) {
-    console.log("record.dynamodb.OldImage");
+    logger.log("record.dynamodb.OldImage");
     return false;
   }
   if (!record.dynamodb.NewImage) {
-    console.log("no record.dynamodb.NewImage");
+    logger.log("no record.dynamodb.NewImage");
     return false;
   }
   if (!record.dynamodb.NewImage[schema.SORT_KEY]) {
-    console.log(`no record.dynamodb.NewImage["${schema.SORT_KEY}"]`);
+    logger.log(`no record.dynamodb.NewImage["${schema.SORT_KEY}"]`);
     return false;
   }
   if (!record.dynamodb.NewImage[schema.SORT_KEY].S) {
-    console.log(`no record.dynamodb.NewImage["${schema.SORT_KEY}"].S`);
+    logger.log(`no record.dynamodb.NewImage["${schema.SORT_KEY}"].S`);
     return false;
   }
   const re = new RegExp(`^${schema.PARTICIPANT_PREFIX}${schema.SEPARATOR}`);
-  console.log("re.test says:");
-  console.log(re.test(record.dynamodb.NewImage[schema.SORT_KEY].S));
+  logger.log("re.test says:");
+  logger.log(re.test(record.dynamodb.NewImage[schema.SORT_KEY].S));
   return re.test(record.dynamodb.NewImage[schema.SORT_KEY].S);
 };
 const handleJoin = () => {
-  console.log("join");
+  logger.log("join");
+  // need to notify participants in this seder
+  // 
 };
 
 exports.handler = async function (event, context, callback) {
   let connections;
-  console.log("event:");
-  console.log(JSON.stringify(event));
-  console.log("context:");
-  console.log(JSON.stringify(context));
+  logger.log("event:");
+  logger.log(JSON.stringify(event));
+  logger.log("context:");
+  logger.log(JSON.stringify(context));
 
-  console.log("event.Records.length:");
-  console.log(event.Records.length);
+  logger.log("event.Records.length:");
+  logger.log(event.Records.length);
 
   for (let r = 0; r < event.Records.length; r++) {
-    console.log("checking record...");
+    logger.log("checking record...");
     if (!event.Records[r].dynamodb.NewImage) {
-      console.log("stream: no NewImage in record");
-      console.log(JSON.stringify(event));
+      logger.log("stream: no NewImage in record");
+      logger.log(JSON.stringify(event));
       continue;
     }
     const record = event.Records[r];

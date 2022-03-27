cad47a63c16d882eaf35b3dcc1c2b3374a46e870
Douglas Naphas <douglasnaphas@gmail.com>
2022-03-26 21:13:30 -0400

    Let the stream handler query the DB
    gh-250
    
    I'm not giving write access for now, because I want to avoid an infinite
    loop situation if I accidentally write to the db from the stream
    handler.

--- lib/madliberation-webapp.ts
+++ lib/madliberation-webapp.ts
@@ -517,6 +517,7 @@ export class MadliberationWebapp extends Stack {
         retryAttempts: 5,
       })
     );
+    sedersTable.grantReadData(streamHandler);
     wsStage.grantManagementApiAccess(streamHandler);
 
     const scriptsBucket = new MadLiberationBucket(this, "ScriptsBucket", {

b98b6abfa3c7eadc97fc0c0d8dd43f1384ee0214
Douglas Naphas <douglasnaphas@gmail.com>
2022-04-17 13:37:37 -0400

    Make YourRoomCodePage error text readable
    gh-416

--- frontend/src/components/YourRoomCodePage.js
+++ frontend/src/components/YourRoomCodePage.js
@@ -137,8 +137,10 @@ class YourRoomCodePage extends Component {
             !this.state.failedAttempt || this.state.thatsMyNameButtonPressed
           }
         >
-          <Typography component="p" color="secondary">
-            {this.state.failureMessage}
+          <Typography component="p">
+            <b>
+              <em>{this.state.failureMessage}</em>
+            </b>
           </Typography>
         </div>
       </div>

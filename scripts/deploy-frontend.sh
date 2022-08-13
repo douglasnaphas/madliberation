dfd8703ad718e99a27e0e901e0ddf324cb7b4d00
Douglas Naphas <douglasnaphas@gmail.com>
2022-08-13 00:18:11 -0400

    Make a note about determining BUCKET
    gh-443

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -3,6 +3,8 @@
 set -e
 
 # get the bucket name from SSM param store
+# we would like to be able to do this only if the bucket is unknown, so that
+# we can save a round trip when deploying from a dev computer
 PARAM_NAME=$(npx @cdk-turnkey/stackname@1.2.0 --suffix FrontendBucketName)
 BUCKET=$(aws ssm get-parameter --name ${PARAM_NAME} | jq '.Parameter.Value' | tr -d '"')
 

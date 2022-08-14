633a3421c83fc024fe27708e3c70a6355b47acb4
Douglas Naphas <douglasnaphas@gmail.com>
2022-08-14 12:19:15 -0400

    Remove outdated backend run script
    It relied on the old SAM stack.

--- backend/run-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Use this to run a Lambda environment locally for development and testing.
-# You need to install AWS's SAM CLI first.
-
-sed 's/NODE_ENV: production/NODE_ENV: development/' ./template.yml > ./dev-template.yml
-sam local start-api --template ./dev-template.yml --port 3002

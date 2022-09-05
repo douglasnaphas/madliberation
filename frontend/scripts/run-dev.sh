24b33d4651d13d4e7ab877b4bf730356cd38b9c6
Douglas Naphas <douglasnaphas@gmail.com>
2022-09-05 16:30:48 -0400

    Make sure mkcert has been run
    gh-443

--- frontend/scripts/run-dev.sh
+++ frontend/scripts/run-dev.sh
@@ -5,4 +5,14 @@ then
     exit 2
 fi
 # Make sure mkcert is set up
+for MKCERT_FILE in localhost{,-key}.pem
+do
+    if [[ ! -f ${MKCERT_FILE} ]]
+    then
+        echo ${MKCERT_FILE} 'must exist in the frontend/ directory.'
+        echo 'Make sure you have run `mkcert -install ; mkcert localhost`, to set'
+        echo 'up TLS for localhost. See https://github.com/FiloSottile/mkcert.'
+        exit 3
+    fi
+done
 docker compose up

9ae8586717011d2c1a7a2966acfef043866ac459
Douglas Naphas <douglasnaphas@gmail.com>
2023-01-20 16:18:21 -0500

    Tag with the SHA even if there's already a SHA tag
    gh-451
    
    My last build failed with:
    
    An error occurred (InvalidTag) when calling the PutBucketTagging
    operation: Cannot provide multiple Tags with the same key

--- frontend/scripts/tag-bucket.sh
+++ frontend/scripts/tag-bucket.sh
@@ -10,6 +10,6 @@ if ! old_tagset=$(aws s3api get-bucket-tagging --bucket $BUCKET 2>/dev/null)
 then
   true
 else
-  new_tagset=$(echo "${old_tagset}" | jq ".TagSet += [{\"Key\":\"SHA\",\"Value\":\"${GITHUB_SHA}\"}]")
+  new_tagset=$(echo "${old_tagset}" | node -e 'const d = JSON.parse(require("fs").readFileSync(0, "utf-8")); const newTagSet = d.TagSet.map(e => {if(e.Name == "SHA") return {Name:"SHA",Value:"'${GITHUB_SHA}'"}; return e;}) ; console.log(JSON.stringify({TagSet: newTagSet}));'
 fi
 aws s3api put-bucket-tagging --bucket ${BUCKET} --tagging "${new_tagset}"

208bc4f41b41ed4899b2033b0ae1b8801cb95953
Douglas Naphas <douglasnaphas@gmail.com>
2023-01-27 18:53:10 -0500

    Show how to use less repetition with styles
    gh-451

--- frontend-create-haggadah/pages/index.tsx
+++ frontend-create-haggadah/pages/index.tsx
@@ -12,41 +12,11 @@ import MadLiberationLogo from "../public/mad-liberation-logo.png";
 import VeryAwesomePassoverLogo from "../public/VAPLogo-white.png";
 import { Global, css, jsx } from "@emotion/react";
 import { Paper } from "@mui/material";
-
-const styles = {
-  ".homePageBackground": {
-    backgroundImage: `url(${RedSeaImage})`,
-    minHeight: "100%",
-    width: "100%",
-    height: "auto",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-  },
-  ".madliberationLogo": {
-    height: "200px",
-  },
-  ".veryAwesomePassoverLogo": {
-    height: "70px",
-  },
-};
+import { madLiberationStyles } from "../madLiberationStyles";
 
 export default function Home() {
   return (
-    <div /*
-      css={{
-        backgroundImage: `url(${RedSeaImage})`,
-        minHeight: "100%",
-        width: "100%",
-        height: "auto",
-        position: "fixed",
-        top: 0,
-        left: 0,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}*/
+    <div 
     >
       <div
         style={{
@@ -64,28 +34,9 @@ export default function Home() {
             src={`${MadLiberationLogo.src}`}
           ></img>
         </div>
-        {/* <Container maxWidth="lg">
-          <Box
-            sx={{
-              my: 4,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h4" component="h1" gutterBottom>
-              MUI v5 + Next.js with TypeScript example
-            </Typography>
-            <Link href="/about" color="secondary">
-              Go to the about page
-            </Link>
-            <ProTip />
-            <Copyright />
-          </Box>
-        </Container> */}
         <Container maxWidth="md">
           <Paper>
+            <div css={madLiberationStyles.lightGrayBackround}>Gray??</div>
             <Typography
               variant="body1"
               component="p"

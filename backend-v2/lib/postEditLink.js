b846a7876196a9c239c50700f23f95e6fcf8fad0
Douglas Naphas <douglasnaphas@gmail.com>
2023-02-28 09:57:47 -0500

    Validate the leaderEmail address
    gh-451

--- backend-v2/lib/postEditLink.js
+++ backend-v2/lib/postEditLink.js
@@ -1,7 +1,17 @@
-const checkBody = require("./checkBody")
+const checkBody = require("./checkBody");
+const validator = require("email-validator");
 const postEditLink = [
   // fail if there's no path and leaderEmail
   checkBody(["path", "leaderEmail"]),
+  // make sure leaderEmail is OK]
+  (req, res, next) => {
+    if (validator.validate(req.body.leaderEmail)) {
+      return next();
+    }
+    return res
+      .status(400)
+      .send({ err: "leaderEmail should be an email address" });
+  },
   // save path and leaderEmail in locals
   // generate password, save in locals
   // generate seder code, save in db w other locals, and in locals

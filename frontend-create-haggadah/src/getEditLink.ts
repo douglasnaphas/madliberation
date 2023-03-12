ae03c1256fe50af69024fcf6a6c1d3d02c77ace3
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-12 00:18:54 -0500

    Follow a redirect from edit-link
    gh-451
    
    https://stackoverflow.com/questions/39735496/redirect-after-a-fetch-post-call

--- frontend-create-haggadah/src/getEditLink.ts
+++ frontend-create-haggadah/src/getEditLink.ts
@@ -2,6 +2,7 @@ const getEditLink = async (props: { path: string; leaderEmail: string }) => {
   const { path, leaderEmail } = props;
   const fetchInit = {
     method: "POST",
+    redirect: "follow",
     body: JSON.stringify({ path, leaderEmail }),
     headers: { "Content-Type": "application/json" },
   };

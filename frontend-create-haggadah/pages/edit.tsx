1fe427d1a5643f2f8240ff7837001dbdd3d5f105
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-11 23:46:15 -0500

    Clear out the edit page

--- frontend-create-haggadah/pages/edit.tsx
+++ frontend-create-haggadah/pages/edit.tsx
@@ -9,84 +9,12 @@ import { Global, css, jsx } from "@emotion/react";
 import { Paper } from "@mui/material";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
-import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
-import MuiAccordionSummary, {
-  AccordionSummaryProps,
-} from "@mui/material/AccordionSummary";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
+
 import { madLiberationStyles } from "../madLiberationStyles";
 import ScriptMenu from "../src/ScriptMenu";
 import { fetchScripts } from "../src/fetchScripts";
 
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  "&:not(:last-child)": {
-    borderBottom: 0,
-  },
-  "&:before": {
-    display: "none",
-  },
-}));
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
-    {...props}
-  />
-))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === "dark"
-      ? "rgba(255, 255, 255, .05)"
-      : "rgba(0, 0, 0, .03)",
-  flexDirection: "row-reverse",
-  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
-    transform: "rotate(90deg)",
-  },
-  "& .MuiAccordionSummary-content": {
-    marginLeft: theme.spacing(1),
-  },
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-  padding: theme.spacing(2),
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-}));
-
-export default function Home() {
-  const [selectedScript, setSelectedScript] = React.useState("");
-  const steps = [
-    {
-      order: 1,
-      label: "Pick script",
-      body: (
-        <div>
-          <ScriptMenu
-            fetchScripts={fetchScripts}
-            selectedScript={selectedScript}
-            setSelectedScript={setSelectedScript}
-          ></ScriptMenu>
-        </div>
-      ),
-    },
-    {
-      order: 2,
-      label: "Your email",
-      body: <div></div>,
-    },
-  ].sort((a: any, b: any) => {
-    if (a.order === b.order) return 0;
-    if (a.order < b.order) return -1;
-    return a;
-  });
-  const [accordionExpanded, setAccordionExpanded] = React.useState(
-    steps.map(() => {
-      return false;
-    })
-  );
-
+export default function Edit() {
   return (
     <div
       style={{
@@ -107,32 +35,7 @@ export default function Home() {
       </div>
       <Container maxWidth="md">
         <Paper>
-          <div>
-            {" "}
-            {/* accordions from steps */}
-            {steps.map((step, index) => {
-              return (
-                <div>
-                  <Accordion
-                    expanded={accordionExpanded[index]}
-                    onChange={(event) => {
-                      setAccordionExpanded((oldAccordionExpanded) => {
-                        return oldAccordionExpanded.map((a, i) => {
-                          if (i === index) {
-                            return !a;
-                          }
-                          return a;
-                        });
-                      });
-                    }}
-                  >
-                    <AccordionSummary>{step.label}</AccordionSummary>
-                    <AccordionDetails>{step.body}</AccordionDetails>
-                  </Accordion>
-                </div>
-              );
-            })}
-          </div>
+          <div></div>
         </Paper>
       </Container>
       <img

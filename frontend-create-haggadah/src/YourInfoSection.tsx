69ce24a759b1ee6bfac2d7af90a65bcb5966bec6
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-19 08:48:03 -0400

    Take name props

--- frontend-create-haggadah/src/YourInfoSection.tsx
+++ frontend-create-haggadah/src/YourInfoSection.tsx
@@ -6,6 +6,8 @@ type YourInfoSectionProps = {
   disabled?: boolean,
   yourEmail: string;
   setYourEmail: React.Dispatch<React.SetStateAction<string>>;
+  yourName?: string;
+  setYourName?: React.Dispatch<React.SetStateAction<string>>;
 };
 const YourInfoSection = (props: YourInfoSectionProps) => {
   const { disabled, yourEmail, setYourEmail } = props;
@@ -13,14 +15,24 @@ const YourInfoSection = (props: YourInfoSectionProps) => {
     <div>
       <div>
         <Typography component="p" paragraph gutterBottom>
-          Enter <label htmlFor="your-email-address">your email address</label>
+          Enter <label htmlFor="your-name">your name</label> and <label htmlFor="your-email-address">your email address</label>
         </Typography>
       </div>
       <div>
+        {yourName && setYourName && (<TextField
+          disabled={disabled}
+          variant="outlined"
+          id="your-name"
+          helperText="Your name"                           
+          onChange={(event) => {
+            setYourName(event.target.value);
+          }}
+        ></TextField>)}
         <TextField
           disabled={disabled}
           variant="outlined"
           id="your-email-address"
+          helperText="Your email address"
           onChange={(event) => {
             setYourEmail(event.target.value);
           }}

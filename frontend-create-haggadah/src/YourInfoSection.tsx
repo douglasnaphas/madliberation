3fe20612ac5bc469fd61480340e93de61b1533b1
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-19 09:07:02 -0400

    Pull yourName, setYourName from props

--- frontend-create-haggadah/src/YourInfoSection.tsx
+++ frontend-create-haggadah/src/YourInfoSection.tsx
@@ -3,31 +3,34 @@ import { useState } from "react";
 import { TextField, Typography } from "@mui/material";
 
 type YourInfoSectionProps = {
-  disabled?: boolean,
+  disabled?: boolean;
   yourEmail: string;
   setYourEmail: React.Dispatch<React.SetStateAction<string>>;
   yourName?: string;
   setYourName?: React.Dispatch<React.SetStateAction<string>>;
 };
 const YourInfoSection = (props: YourInfoSectionProps) => {
-  const { disabled, yourEmail, setYourEmail } = props;
+  const { disabled, yourEmail, setYourEmail, yourName, setYourName } = props;
   return (
     <div>
       <div>
         <Typography component="p" paragraph gutterBottom>
-          Enter <label htmlFor="your-name">your name</label> and <label htmlFor="your-email-address">your email address</label>
+          Enter <label htmlFor="your-name">your name</label> and{" "}
+          <label htmlFor="your-email-address">your email address</label>
         </Typography>
       </div>
       <div>
-        {yourName && setYourName && (<TextField
-          disabled={disabled}
-          variant="outlined"
-          id="your-name"
-          helperText="Your name"                           
-          onChange={(event) => {
-            setYourName(event.target.value);
-          }}
-        ></TextField>)}
+        {yourName && setYourName && (
+          <TextField
+            disabled={disabled}
+            variant="outlined"
+            id="your-name"
+            helperText="Your name"
+            onChange={(event) => {
+              setYourName(event.target.value);
+            }}
+          ></TextField>
+        )}
         <TextField
           disabled={disabled}
           variant="outlined"

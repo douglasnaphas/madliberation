df2239c9633ea2bfb7701373a1bcb961767c02e5
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-27 16:21:56 -0400

    Add a blank read page that tries to get the script
    gh-497

---
+++ frontend-create-haggadah/pages/read.tsx
@@ -0,0 +1,84 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import MadLiberationLogo from "../public/mad-liberation-logo.png";
+import VeryAwesomePassoverLogo from "../public/VAPLogo-white.png";
+import { Global, css, jsx } from "@emotion/react";
+import { Button, Paper, TextField } from "@mui/material";
+
+const enum PageState {
+  LOADING = 0,
+  READY,
+  FETCHING,
+}
+export default function Read() {
+  const [pageState, setPageState] = React.useState(PageState.LOADING);
+
+  let sederCode: any, pw: any, hashPage: any;
+  if (typeof window !== "undefined" && typeof URLSearchParams === "function") {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    sederCode = urlSearchParams.get("sederCode");
+    pw = urlSearchParams.get("pw");
+    if (
+      window.location.hash.split("#").length > 1 &&
+      parseInt(window.location.hash.split("#")[1])
+    ) {
+      hashPage = parseInt(window.location.hash.split("#")[1]);
+    }
+  }
+  const [selectedPage, setSelectedPage] = React.useState();
+  const [script, setScript] = React.useState({});
+
+  React.useEffect(() => {
+    (async () => {
+      if (sederCode && pw) {
+        const fetchScriptResponse = await fetch(
+          `../v2/script?sederCode=${sederCode}&pw=${pw}&roomcode=${sederCode}`
+        );
+        if (fetchScriptResponse.status !== 200) {
+          return;
+        }
+        const fetchScriptData = await fetchScriptResponse.json();
+        setScript(fetchScriptData);
+      }
+    })();
+  }, []);
+  return (
+    <div
+      style={{
+        backgroundColor: "#81181f",
+        height: "100%",
+        minHeight: "100%",
+      }}
+    >
+      <div>
+        <img
+          css={{
+            height: "200px",
+            display: "block",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+          src={`${MadLiberationLogo.src}`}
+        ></img>
+      </div>
+      <Container maxWidth="md">
+        <Paper>
+          <div style={{ padding: "8px" }}></div>
+        </Paper>
+      </Container>
+      <br></br>
+      <img
+        css={{
+          height: "70px",
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        src={`${VeryAwesomePassoverLogo.src}`}
+      ></img>
+    </div>
+  );
+}

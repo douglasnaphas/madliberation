1ab522a71b447c2f351b1d7d8874eb3902a7bb78
Douglas Naphas <douglasnaphas@gmail.com>
2023-03-28 08:54:55 -0400

    Format saveSederInfo

--- backend-v2/lib/scriptMiddleware/saveSederInfo.js
+++ backend-v2/lib/scriptMiddleware/saveSederInfo.js
@@ -20,48 +20,50 @@
  */
 function saveSederInfo() {
   const middleware = (req, res, next) => {
-    const responses = require('../../responses');
-    const schema = require('../../schema');
-    if(!res.locals.dbData ||
+    const responses = require("../../responses");
+    const schema = require("../../schema");
+    if (
+      !res.locals.dbData ||
       !Array.isArray(res.locals.dbData.Items) ||
-      res.locals.dbData.Items.length < 2) {
+      res.locals.dbData.Items.length < 2
+    ) {
       return res.status(500).send(responses.SERVER_ERROR);
     }
     const items = res.locals.dbData.Items;
     res.locals.path = res.locals.version = false;
     res.locals.answers = [];
-    for(let i = 0; i < items.length; i++) {
+    for (let i = 0; i < items.length; i++) {
       res.locals.path = res.locals.path || items[i][schema.PATH];
-      res.locals.version = res.locals.version ||
-        items[i][schema.SCRIPT_VERSION];
+      res.locals.version =
+        res.locals.version || items[i][schema.SCRIPT_VERSION];
       const answers = [];
-      if(Array.isArray(items[i].answers)) {
+      if (Array.isArray(items[i].answers)) {
         // make it easy to look up answers by id
-        for(let j = 0; j < items[i].answers.length; j++) {
-          if(Number.isInteger(items[i].answers[j].id)) {
-            answers[items[i].answers[j].id] =
-              items[i].answers[j].answer;
+        for (let j = 0; j < items[i].answers.length; j++) {
+          if (Number.isInteger(items[i].answers[j].id)) {
+            answers[items[i].answers[j].id] = items[i].answers[j].answer;
           }
         }
       }
-      if(Array.isArray(items[i].assignments)) {
-        for(let j = 0; j < items[i].assignments.length; j++) {
-          if(Number.isInteger(items[i].assignments[j].id)) {
+      if (Array.isArray(items[i].assignments)) {
+        for (let j = 0; j < items[i].assignments.length; j++) {
+          if (Number.isInteger(items[i].assignments[j].id)) {
             res.locals.answers.push({
               id: items[i].assignments[j].id,
-              answer: answers[items[i].assignments[j].id] ||
-                items[i].assignments[j].defaultAnswer
+              answer:
+                answers[items[i].assignments[j].id] ||
+                items[i].assignments[j].defaultAnswer,
             });
           }
         }
       }
     }
     res.locals.answers.sort((a, b) => {
-      if(a.id < b.id) return -1;
-      if(a.id > b.id) return 1;
+      if (a.id < b.id) return -1;
+      if (a.id > b.id) return 1;
       return 0;
     });
-    if(!res.locals.path || !res.locals.version) {
+    if (!res.locals.path || !res.locals.version) {
       return res.status(500).send(responses.SERVER_ERROR);
     }
     return next();

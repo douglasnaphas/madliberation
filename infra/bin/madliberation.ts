c392aba8df0bfc000bd2fa676aec74a655ed106c
Douglas Naphas <douglasnaphas@gmail.com>
2023-05-08 00:55:40 -0400

    Use aws sdk v3 in infra/bin
    gh-487

--- infra/bin/madliberation.ts
+++ infra/bin/madliberation.ts
@@ -42,45 +42,6 @@ const stackname = require("@cdk-turnkey/stackname");
     new ConfigParam("googleClientId"),
     new ConfigParam("googleClientSecret"),
   ];
-
-  const ssmParams = {
-    Names: configParams.map((c) => c.ssmParamName()),
-    WithDecryption: true,
-  };
-
-  AWS.config.update({ region: process.env.AWS_DEFAULT_REGION });
-  const ssm = new AWS.SSM();
-  let ssmResponse: any;
-  ssmResponse = await new Promise((resolve, reject) => {
-    ssm.getParameters(ssmParams, (err: any, data: any) => {
-      resolve({ err, data });
-    });
-  });
-
-  if (!ssmResponse.data) {
-    console.log("error: unsuccessful SSM call, failing");
-    console.log(ssmResponse);
-    process.exit(1);
-  }
-
-  const ssmParameterData: any = {};
-  let valueHash;
-  ssmResponse?.data?.Parameters?.forEach(
-    (p: { Name: string; Value: string }) => {
-      console.log("Received parameter named:");
-      console.log(p.Name);
-      valueHash = crypto
-        .createHash("sha256")
-        .update(p.Value)
-        .digest("hex")
-        .toLowerCase();
-      console.log("value hash:");
-      console.log(valueHash);
-      console.log("**************");
-      ssmParameterData[p.Name] = p.Value;
-    }
-  );
-
   const ssmClient = new SSMClient({ region: process.env.AWS_DEFAULT_REGION });
   const getParametersInput = {
     Names: configParams.map((c) => c.ssmParamName()),
@@ -88,6 +49,8 @@ const stackname = require("@cdk-turnkey/stackname");
   };
   const getParametersCommand = new GetParametersCommand(getParametersInput);
   const getParametersResponse = await ssmClient.send(getParametersCommand);
+  const ssmParameterData: any = {};
+  let valueHash;
   getParametersResponse?.Parameters?.forEach(
     (p: { Name: string; Value: string }) => {
       console.log("(v3) Received parameter named:");
@@ -103,9 +66,7 @@ const stackname = require("@cdk-turnkey/stackname");
       ssmParameterData[p.Name] = p.Value;
     }
   );
-
   console.log("==================");
-
   configParams.forEach((c) => {
     c.ssmParamValue = ssmParameterData[c.ssmParamName()];
   });

f8e01eaf5ef3e8170d01d4eb136194611cb36ca9
Douglas Naphas <douglasnaphas@gmail.com>
2024-03-17 10:47:19 -0400

    Allow any element for submit button subtext

--- itest/Links.itest.cjs
+++ itest/Links.itest.cjs
@@ -440,8 +440,8 @@ const waitOptions = { timeout: timeoutMs /*, visible: true*/ };
     // click submit
     const buttonExplanationXPath =
       asi === browserUser.assignments.length - 1
-        ? '//div[text()="Submit this one"]'
-        : '//div[contains(text(),"Submit, go to next prompt")]';
+        ? '//*[text()="Submit this one"]'
+        : '//*[contains(text(),"Submit, go to next prompt")]';
     await page.waitForXPath(buttonExplanationXPath);
     const submitButtonXPath =
       `//button[contains(text(),"Submit"][not(@disabled)]`;

571bd1047fa12a2ebe424155f5340d553dceea3f
Douglas Naphas <douglasnaphas@gmail.com>
2024-04-03 22:17:15 -0400

    Shrink the home page explanatory heading

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -58,6 +58,20 @@ class HomePage extends Component {
               />
             </div>
             <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div id="site-description">
+                <h1 style={{ fontSize: "16px", color: "white" }}>
+                  Mad lib Haggadahs for your Passover Seder
+                </h1>
+              </div>
+            </div>
+            <br></br>
+            <div
               id="home-page-main-content"
               style={{
                 display: "flex",
@@ -65,94 +79,93 @@ class HomePage extends Component {
                 alignItems: "center",
               }}
             >
-              <Paper style={{ padding: "8px", maxWidth: "fit-content" }}>
-                <div id="site-description">
-                  <h1>Mad lib Haggadahs for your Passover Seder</h1>
-                </div>
-                <div>
-                  <Button
-                    madliberationid="plan-seder-button"
-                    variant="contained"
-                    color="primary"
-                    href={createHaggadahHref}
-                    disabled={!user}
-                  >
-                    {createHaggadahLinkText}
-                  </Button>
-                </div>
-                <br />
-                {!user && (
-                  <div id="login-container">
-                    <a href={Configs.loginUrl()} className="loginLink">
-                      <Button
-                        madliberationid="login-button"
-                        title="Log in"
-                        variant="contained"
-                        color="secondary"
-                      >
-                        Log in
-                      </Button>
-                    </a>
-                  </div>
-                )}
-                <br />
-
-                {user && (
-                  <>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
-                      id="logout-container"
+              <div>
+                <Paper style={{ padding: "8px", maxWidth: "fit-content" }}>
+                  <div>
+                    <Button
+                      madliberationid="plan-seder-button"
+                      variant="contained"
+                      color="primary"
+                      href={createHaggadahHref}
+                      disabled={!user}
                     >
-                      <br />
-                      <div id="post-login-content">
-                        <Typography component="p">
-                          Logged in as {user.nickname}
-                        </Typography>
-                        <div>
-                          <Typography component="p">
-                            <Button
-                              component={Link}
-                              to="/seders"
-                              title="see-your-seders-button"
-                              madliberationid="see-your-seders-button"
-                            >
-                              See your seders
-                            </Button>
-                          </Typography>
-                        </div>
-                        <div>
-                          <Typography component="p">
-                            <Button
-                              disabled={this.state.logoutClicked}
-                              onClick={() => {
-                                this.setState({ logoutClicked: true });
-                                fetch(Configs.apiRelativeUrl("logout"), {
-                                  credentials: "include",
-                                }).then((r) => {
-                                  setUser(false);
-                                  storage.removeItem("user-nickname");
-                                  storage.removeItem("user-email");
-                                });
-                              }}
-                              madliberationid="logout-button"
-                            >
-                              Log out
-                            </Button>
-                          </Typography>
-                        </div>
-                      </div>{" "}
+                      {createHaggadahLinkText}
+                    </Button>
+                  </div>
+                  <br />
+                  {!user && (
+                    <div id="login-container">
+                      <a href={Configs.loginUrl()} className="loginLink">
+                        <Button
+                          madliberationid="login-button"
+                          title="Log in"
+                          variant="contained"
+                          color="secondary"
+                        >
+                          Log in
+                        </Button>
+                      </a>
                     </div>
-                  </>
-                )}
-                <div>
-                  <Typography component="p">
-                    <a href="#/about">About</a>
-                  </Typography>
-                </div>
-              </Paper>
+                  )}
+                  <br />
+
+                  {user && (
+                    <>
+                      <div
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                        }}
+                        id="logout-container"
+                      >
+                        <br />
+                        <div id="post-login-content">
+                          <Typography component="p">
+                            Logged in as {user.nickname}
+                          </Typography>
+                          <div>
+                            <Typography component="p">
+                              <Button
+                                component={Link}
+                                to="/seders"
+                                title="see-your-seders-button"
+                                madliberationid="see-your-seders-button"
+                              >
+                                See your seders
+                              </Button>
+                            </Typography>
+                          </div>
+                          <div>
+                            <Typography component="p">
+                              <Button
+                                disabled={this.state.logoutClicked}
+                                onClick={() => {
+                                  this.setState({ logoutClicked: true });
+                                  fetch(Configs.apiRelativeUrl("logout"), {
+                                    credentials: "include",
+                                  }).then((r) => {
+                                    setUser(false);
+                                    storage.removeItem("user-nickname");
+                                    storage.removeItem("user-email");
+                                  });
+                                }}
+                                madliberationid="logout-button"
+                              >
+                                Log out
+                              </Button>
+                            </Typography>
+                          </div>
+                        </div>{" "}
+                      </div>
+                    </>
+                  )}
+                  <div>
+                    <Typography component="p">
+                      <a href="#/about">About</a>
+                    </Typography>
+                  </div>
+                </Paper>
+              </div>
             </div>{" "}
             <br />
             <br />

19844e2f3c1f09402f0bdc2d673efd93dfddd4a7
Douglas Naphas <douglasnaphas@gmail.com>
2024-04-21 09:53:24 -0400

    Treat Records as an array
    gh-577
    
    https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Streams.Tutorial.html

--- backend-v2/submit.js
+++ backend-v2/submit.js
@@ -9,56 +9,57 @@ const {
 } = require("@aws-sdk/client-apigatewaymanagementapi");
 const schema = require("./schema");
 
-exports.handler = async function (record) {
-  console.log("record", record);
-  console.log(JSON.stringify(record));
-  console.log("dynamodb", JSON.stringify(record.dynamodb));
-  const sederCode = record.dynamodb.NewImage[schema.PARTITION_KEY].S;
-  const region = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION;
-  const ddbClient = new DynamoDBClient({ region });
-  const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
-  const apiGwClient = new ApiGatewayManagementApiClient({ region });
+exports.handler = async function (event) {
+  console.log("event", JSON.stringify(event));
+  for (let r = 0; r < event.Records.length; r++) {
+    const record = event.Records[r];
+    const sederCode = record.dynamodb.NewImage[schema.PARTITION_KEY].S;
+    const region = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION;
+    const ddbClient = new DynamoDBClient({ region });
+    const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
+    const apiGwClient = new ApiGatewayManagementApiClient({ region });
 
-  // get all the read connections
-  const queryForReadConnectionsCommand = new QueryCommand({
-    TableName: schema.TABLE_NAME,
-    IndexName: "GSI1",
-    KeyConditionExpression: `GSI1PK = :pk`,
-    ExpressionAttributeValues: {
-      ":pk": `${schema.SEDER_CODE}${schema.SEPARATOR}${sederCode}`,
-    },
-  });
-  const queryForReadConnectionsResponse = await ddbDocClient.send(
-    queryForReadConnectionsCommand
-  );
-  if (!queryForReadConnectionsResponse) {
-    console.log("failed to query for read connections");
-    console.log(queryForReadConnectionsResponse);
-  }
-  if (!queryForReadConnectionsResponse.Items) {
-    console.log("No Items");
-  }
+    // get all the read connections
+    const queryForReadConnectionsCommand = new QueryCommand({
+      TableName: schema.TABLE_NAME,
+      IndexName: "GSI1",
+      KeyConditionExpression: `GSI1PK = :pk`,
+      ExpressionAttributeValues: {
+        ":pk": `${schema.SEDER_CODE}${schema.SEPARATOR}${sederCode}`,
+      },
+    });
+    const queryForReadConnectionsResponse = await ddbDocClient.send(
+      queryForReadConnectionsCommand
+    );
+    if (!queryForReadConnectionsResponse) {
+      console.log("failed to query for read connections");
+      console.log(queryForReadConnectionsResponse);
+    }
+    if (!queryForReadConnectionsResponse.Items) {
+      console.log("No Items");
+    }
 
-  // for each one, let them know to re-fetch the script
-  if (
-    queryForReadConnectionsResponse &&
-    Array.isArray(queryForReadConnectionsResponse.Items)
-  ) {
-    const readConnections = queryForReadConnectionsResponse.Items;
-    for (let c = 0; c < readConnections.length; c++) {
-      const { ConnectionId } = readConnections[c];
-      console.log(`notifying ${ConnectionId}`);
-      const postToConnectionRequest = {
-        Data: Buffer.from("answer submitted"),
-        ConnectionId,
-      };
-      const postToConnectionCommand = new PostToConnectionCommand(
-        postToConnectionRequest
-      );
-      const postToConnectionResponse = await client.send(
-        postToConnectionCommand
-      );
-      console.log(postToConnectionResponse);
+    // for each one, let them know to re-fetch the script
+    if (
+      queryForReadConnectionsResponse &&
+      Array.isArray(queryForReadConnectionsResponse.Items)
+    ) {
+      const readConnections = queryForReadConnectionsResponse.Items;
+      for (let c = 0; c < readConnections.length; c++) {
+        const { ConnectionId } = readConnections[c];
+        console.log(`notifying ${ConnectionId}`);
+        const postToConnectionRequest = {
+          Data: Buffer.from("answer submitted"),
+          ConnectionId,
+        };
+        const postToConnectionCommand = new PostToConnectionCommand(
+          postToConnectionRequest
+        );
+        const postToConnectionResponse = await client.send(
+          postToConnectionCommand
+        );
+        console.log(postToConnectionResponse);
+      }
     }
   }
 };
